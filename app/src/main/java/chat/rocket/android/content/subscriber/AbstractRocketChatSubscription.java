33650253e49824a9aad6508d43a51f647f74aa13
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2015-12-01 19:39:52 +0900

    refactor subscribers

---
+++ app/src/main/java/chat/rocket/android/content/subscriber/AbstractRocketChatSubscription.java
@@ -0,0 +1,98 @@
+package chat.rocket.android.content.subscriber;
+
+import android.content.Context;
+import android.os.Looper;
+import android.text.TextUtils;
+import android.util.Log;
+
+import org.json.JSONException;
+
+import bolts.Continuation;
+import bolts.Task;
+import chat.rocket.android.api.ws.RocketChatWSAPI;
+import jp.co.crowdworks.android_ddp.ddp.DDPSubscription;
+import rx.Subscription;
+import rx.functions.Action1;
+import rx.functions.Func1;
+
+abstract class AbstractRocketChatSubscription extends AbstractSubscriber {
+
+    private String mID;
+    private Subscription mSubscription;
+
+    public AbstractRocketChatSubscription(Context context, Looper looper, RocketChatWSAPI api) {
+        super(context, looper, api);
+    }
+
+    protected abstract String getSubscriptionName();
+    protected abstract String getSubscriptionCallbackName();
+    protected abstract void onDocumentAdded(DDPSubscription.DocEvent docEvent) throws JSONException;
+
+    @Override
+    protected void onSubscribe() {
+        mAPI.subscribe(getSubscriptionName(),null).onSuccess(new Continuation<DDPSubscription.Ready, Object>() {
+            @Override
+            public Object then(Task<DDPSubscription.Ready> task) throws Exception {
+                mID = task.getResult().id;
+
+                return null;
+            }
+        }).continueWith(new Continuation<Object, Object>() {
+            @Override
+            public Object then(Task<Object> task) throws Exception {
+                if(task.isFaulted()){
+                    Log.e(TAG, "error", task.getError());
+                }
+                return null;
+            }
+        });
+
+        registerSubscriptionCallback();
+    }
+
+    private void registerSubscriptionCallback(){
+        mSubscription = mAPI.getSubscriptionCallback()
+                .filter(new Func1<DDPSubscription.Event, Boolean>() {
+                    @Override
+                    public Boolean call(DDPSubscription.Event event) {
+                        return event instanceof DDPSubscription.DocEvent
+                                && getSubscriptionCallbackName().equals(((DDPSubscription.DocEvent) event).collection);
+                    }
+                })
+                .cast(DDPSubscription.DocEvent.class)
+                .subscribe(new Action1<DDPSubscription.DocEvent>() {
+                    @Override
+                    public void call(DDPSubscription.DocEvent docEvent) {
+                        try {
+                            if (docEvent instanceof DDPSubscription.Added.Before) {
+
+                            } else if (docEvent instanceof DDPSubscription.Added) {
+                                onDocumentAdded(docEvent);
+                            } else if (docEvent instanceof DDPSubscription.Removed) {
+                            } else if (docEvent instanceof DDPSubscription.Changed) {
+
+                            } else if (docEvent instanceof DDPSubscription.MovedBefore) {
+
+                            }
+                        } catch (Exception e) {
+                            Log.e(TAG, "error", e);
+                        }
+
+                    }
+                });
+
+    }
+
+    @Override
+    protected void onUnsubscribe() {
+        if(mSubscription!=null) mSubscription.unsubscribe();
+        if(!TextUtils.isEmpty(mID)) {
+            mAPI.unsubscribe(mID).continueWith(new Continuation<DDPSubscription.NoSub, Object>() {
+                @Override
+                public Object then(Task<DDPSubscription.NoSub> task) throws Exception {
+                    return null;
+                }
+            });
+        }
+    }
+}
--- app/src/main/java/chat/rocket/android/content/subscriber/RocketChatSubscription.java
+++ app/src/main/java/chat/rocket/android/content/subscriber/RocketChatSubscription.java
@@ -3,116 +3,48 @@ package chat.rocket.android.content.subscriber;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Looper;
-import android.text.TextUtils;
-import android.util.Log;
 
-import java.util.HashMap;
+import org.json.JSONException;
 
-import bolts.Continuation;
-import bolts.Task;
 import chat.rocket.android.api.ws.RocketChatWSAPI;
 import chat.rocket.android.content.RocketChatDatabaseHelper;
-import chat.rocket.android.model.RocketChatDocument;
 import chat.rocket.android.model.Room;
 import jp.co.crowdworks.android_ddp.ddp.DDPSubscription;
-import rx.Subscription;
-import rx.functions.Action1;
-import rx.functions.Func1;
 
-public class RocketChatSubscription extends AbstractSubscriber {
-
-    private HashMap<String, RocketChatDocument> mDocumentStore;
-    private String mID;
-    private Subscription mSubscription;
+public class RocketChatSubscription extends AbstractRocketChatSubscription {
 
     public RocketChatSubscription(Context context, Looper looper, RocketChatWSAPI api) {
         super(context, looper, api);
     }
 
     @Override
-    protected void onSubscribe() {
-        mDocumentStore = new HashMap<String, RocketChatDocument>();
-        mAPI.subscribe("subscription",null).onSuccess(new Continuation<DDPSubscription.Ready, Object>() {
-            @Override
-            public Object then(Task<DDPSubscription.Ready> task) throws Exception {
-                mID = task.getResult().id;
-
-                return null;
-            }
-        }).continueWith(new Continuation<Object, Object>() {
-            @Override
-            public Object then(Task<Object> task) throws Exception {
-                if(task.isFaulted()){
-                    Log.e(TAG, "error", task.getError());
-                }
-                return null;
-            }
-        });
-
-        registerSubscriptionCallback();
-    }
-
-    private void registerSubscriptionCallback(){
-        mSubscription = mAPI.getSubscriptionCallback()
-                .filter(new Func1<DDPSubscription.Event, Boolean>() {
-                    @Override
-                    public Boolean call(DDPSubscription.Event event) {
-                        return event instanceof DDPSubscription.DocEvent
-                                && "rocketchat_subscription".equals(((DDPSubscription.DocEvent) event).collection);
-                    }
-                })
-                .cast(DDPSubscription.DocEvent.class)
-                .subscribe(new Action1<DDPSubscription.DocEvent>() {
-                    @Override
-                    public void call(DDPSubscription.DocEvent docEvent) {
-                        try {
-                            if (docEvent instanceof DDPSubscription.Added.Before) {
-
-                            } else if (docEvent instanceof DDPSubscription.Added) {
-                                final String roomID = ((DDPSubscription.Added) docEvent).fields.getString("rid");
-                                String roomName = ((DDPSubscription.Added) docEvent).fields.getString("name");
-                                long roomTs = ((DDPSubscription.Added) docEvent).fields.getJSONObject("ts").getLong("$date");
-
-                                mDocumentStore.put(docEvent.docID, new RocketChatDocument(docEvent.docID, Room.class, roomID));
-
-                                Room r = RocketChatDatabaseHelper.read(mContext, new RocketChatDatabaseHelper.DBCallback<Room>() {
-                                    @Override
-                                    public Room process(SQLiteDatabase db) throws Exception {
-                                        return Room.getById(db, roomID);
-                                    }
-                                });
-                                if(r==null) {
-                                    r = new Room();
-                                    r.id = roomID;
-                                }
-                                r.name = roomName;
-                                r.timestamp = roomTs;
-                                r.putByContentProvider(mContext);
-                            } else if (docEvent instanceof DDPSubscription.Removed) {
-                            } else if (docEvent instanceof DDPSubscription.Changed) {
-
-                            } else if (docEvent instanceof DDPSubscription.MovedBefore) {
-
-                            }
-                        } catch (Exception e) {
-                            Log.e(TAG, "error", e);
-                        }
-
-                    }
-                });
-
+    protected String getSubscriptionName() {
+        return "subscription";
     }
 
     @Override
-    protected void onUnsubscribe() {
-        if(mSubscription!=null) mSubscription.unsubscribe();
-        if(!TextUtils.isEmpty(mID)) {
-            mAPI.unsubscribe(mID).continueWith(new Continuation<DDPSubscription.NoSub, Object>() {
-                @Override
-                public Object then(Task<DDPSubscription.NoSub> task) throws Exception {
-                    return null;
-                }
-            });
+    protected String getSubscriptionCallbackName() {
+        return "rocketchat_subscription";
+    }
+
+    @Override
+    protected void onDocumentAdded(DDPSubscription.DocEvent docEvent) throws JSONException {
+        final String roomID = ((DDPSubscription.Added) docEvent).fields.getString("rid");
+        String roomName = ((DDPSubscription.Added) docEvent).fields.getString("name");
+        long roomTs = ((DDPSubscription.Added) docEvent).fields.getJSONObject("ts").getLong("$date");
+
+        Room r = RocketChatDatabaseHelper.read(mContext, new RocketChatDatabaseHelper.DBCallback<Room>() {
+            @Override
+            public Room process(SQLiteDatabase db) throws Exception {
+                return Room.getById(db, roomID);
+            }
+        });
+        if(r==null) {
+            r = new Room();
+            r.id = roomID;
         }
+        r.name = roomName;
+        r.timestamp = roomTs;
+        r.putByContentProvider(mContext);
     }
 }
