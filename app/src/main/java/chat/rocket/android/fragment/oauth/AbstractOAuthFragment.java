5661f5aa8c096a6fcde9226d73fa1704e0dc4a62
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2016-12-07 21:28:19 +0900

    Separate common logic of {GitHub/Twitter}OAuthFragment into AbstractOAuthFragment.

---
+++ app/src/main/java/chat/rocket/android/fragment/oauth/AbstractOAuthFragment.java
@@ -0,0 +1,128 @@
+package chat.rocket.android.fragment.oauth;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.text.TextUtils;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import chat.rocket.android.api.MethodCallHelper;
+import chat.rocket.android.fragment.AbstractWebViewFragment;
+import chat.rocket.android.helper.LogcatIfError;
+import chat.rocket.android.model.ServerConfig;
+import chat.rocket.android.model.ddp.MeteorLoginServiceConfiguration;
+import chat.rocket.android.realm_helper.RealmStore;
+import org.json.JSONException;
+import org.json.JSONObject;
+import timber.log.Timber;
+
+abstract class AbstractOAuthFragment extends AbstractWebViewFragment {
+
+  protected String serverConfigId;
+  protected String hostname;
+  private String url;
+  private boolean resultOK;
+
+  protected abstract String getOAuthServiceName();
+
+  protected abstract String generateURL(MeteorLoginServiceConfiguration oauthConfig);
+
+  private boolean hasValidArgs(Bundle args) {
+    return args != null
+        && args.containsKey("serverConfigId");
+  }
+
+  @Override public void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Bundle args = getArguments();
+    if (!hasValidArgs(args)) {
+      throw new IllegalArgumentException(
+          "serverConfigId required");
+    }
+
+    serverConfigId = args.getString("serverConfigId");
+    ServerConfig serverConfig = RealmStore.getDefault().executeTransactionForRead(realm ->
+        realm.where(ServerConfig.class).equalTo("serverConfigId", serverConfigId).findFirst());
+    MeteorLoginServiceConfiguration oauthConfig =
+        RealmStore.get(serverConfigId).executeTransactionForRead(realm ->
+            realm.where(MeteorLoginServiceConfiguration.class)
+                .equalTo("service", getOAuthServiceName())
+                .findFirst());
+    if (serverConfig == null || oauthConfig == null) {
+      throw new IllegalArgumentException(
+          "Invalid serverConfigId given,");
+    }
+    hostname = serverConfig.getHostname();
+    url = generateURL(oauthConfig);
+  }
+
+  @Override protected void navigateToInitialPage(WebView webview) {
+    if (TextUtils.isEmpty(url)) {
+      finish();
+      return;
+    }
+
+    resultOK = false;
+    webview.loadUrl(url);
+    webview.addJavascriptInterface(new JSInterface(result -> {
+      // onPageFinish is called twice... Should ignore latter one.
+      if (resultOK) {
+        return;
+      }
+
+      if (result != null && result.optBoolean("setCredentialToken", false)) {
+        try {
+          final String credentialToken = result.getString("credentialToken");
+          final String credentialSecret = result.getString("credentialSecret");
+
+          handleOAuthCallback(credentialToken, credentialSecret);
+          resultOK = true;
+        } catch (JSONException exception) {
+          Timber.e(exception, "failed to parse OAuth result.");
+        }
+      }
+
+      onOAuthCompleted();
+    }), "_rocketchet_hook");
+  }
+
+  @Override protected void onPageLoaded(WebView webview, String url) {
+    super.onPageLoaded(webview, url);
+
+
+    if (url.contains(hostname) && url.contains("_oauth/" + getOAuthServiceName() + "?close")) {
+      final String jsHookUrl = "javascript:"
+          + "window._rocketchet_hook.handleConfig(document.getElementById('config').innerText);";
+      webview.loadUrl(jsHookUrl);
+    }
+  }
+
+  private interface JSInterfaceCallback {
+    void hanldeResult(@Nullable JSONObject result);
+  }
+
+  private static final class JSInterface {
+    private final JSInterfaceCallback jsInterfaceCallback;
+
+    JSInterface(JSInterfaceCallback callback) {
+      jsInterfaceCallback = callback;
+    }
+
+    @JavascriptInterface public void handleConfig(String config) {
+      try {
+        jsInterfaceCallback.hanldeResult(new JSONObject(config));
+      } catch (Exception exception) {
+        jsInterfaceCallback.hanldeResult(null);
+      }
+    }
+  }
+
+  private void handleOAuthCallback(final String credentialToken, final String credentialSecret) {
+    new MethodCallHelper(getContext(), serverConfigId)
+        .loginWithOAuth(credentialToken, credentialSecret)
+        .continueWith(new LogcatIfError());
+  }
+
+  protected void onOAuthCompleted() {
+
+  }
+}
--- app/src/main/java/chat/rocket/android/fragment/oauth/TwitterOAuthFragment.java
+++ app/src/main/java/chat/rocket/android/fragment/oauth/TwitterOAuthFragment.java
@@ -1,29 +1,13 @@
 package chat.rocket.android.fragment.oauth;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.text.TextUtils;
 import android.util.Base64;
-import android.webkit.JavascriptInterface;
-import android.webkit.WebView;
-import chat.rocket.android.api.MethodCallHelper;
-import chat.rocket.android.fragment.AbstractWebViewFragment;
-import chat.rocket.android.helper.LogcatIfError;
-import chat.rocket.android.model.ServerConfig;
 import chat.rocket.android.model.ddp.MeteorLoginServiceConfiguration;
-import chat.rocket.android.realm_helper.RealmStore;
-import hugo.weaving.DebugLog;
 import java.nio.charset.Charset;
-import org.json.JSONException;
 import org.json.JSONObject;
 import timber.log.Timber;
 
-public class TwitterOAuthFragment extends AbstractWebViewFragment {
-
-  private String serverConfigId;
-  private String hostname;
-  private String url;
-  private boolean resultOK;
+public class TwitterOAuthFragment extends AbstractOAuthFragment {
 
   /**
    * create new Fragment with ServerConfig-ID.
@@ -36,36 +20,11 @@ public class TwitterOAuthFragment extends AbstractWebViewFragment {
     return fragment;
   }
 
-  private boolean hasValidArgs(Bundle args) {
-    return args != null
-        && args.containsKey("serverConfigId");
+  @Override protected String getOAuthServiceName() {
+    return "twitter";
   }
 
-  @Override public void onCreate(@Nullable Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    Bundle args = getArguments();
-    if (!hasValidArgs(args)) {
-      throw new IllegalArgumentException(
-          "serverConfigId required");
-    }
-
-    serverConfigId = args.getString("serverConfigId");
-    ServerConfig serverConfig = RealmStore.getDefault().executeTransactionForRead(realm ->
-        realm.where(ServerConfig.class).equalTo("serverConfigId", serverConfigId).findFirst());
-    MeteorLoginServiceConfiguration oauthConfig =
-        RealmStore.get(serverConfigId).executeTransactionForRead(realm ->
-            realm.where(MeteorLoginServiceConfiguration.class)
-                .equalTo("service", "twitter")
-                .findFirst());
-    if (serverConfig == null || oauthConfig == null) {
-      throw new IllegalArgumentException(
-          "Invalid serverConfigId given,");
-    }
-    hostname = serverConfig.getHostname();
-    url = generateURL();
-  }
-
-  private String generateURL() {
+  @Override protected String generateURL(MeteorLoginServiceConfiguration oauthConfig) {
     try {
       String state = Base64.encodeToString(new JSONObject().put("loginStyle", "popup")
           .put("credentialToken", "twitter" + System.currentTimeMillis())
@@ -78,77 +37,5 @@ public class TwitterOAuthFragment extends AbstractWebViewFragment {
       Timber.e(exception, "failed to generate Twitter OAUth URL");
     }
     return null;
-  }
-
-  @Override protected void navigateToInitialPage(WebView webview) {
-    if (TextUtils.isEmpty(url)) {
-      finish();
-      return;
-    }
-
-    resultOK = false;
-    webview.loadUrl(url);
-    webview.addJavascriptInterface(new JSInterface(result -> {
-      // onPageFinish is called twice... Should ignore latter one.
-      if (resultOK) {
-        return;
-      }
-
-      if (result != null && result.optBoolean("setCredentialToken", false)) {
-        try {
-          final String credentialToken = result.getString("credentialToken");
-          final String credentialSecret = result.getString("credentialSecret");
-
-          handleOAuthCallback(credentialToken, credentialSecret);
-          resultOK = true;
-        } catch (JSONException exception) {
-          Timber.e(exception, "failed to parse OAuth result.");
-        }
-      }
-
-      onOAuthCompleted();
-    }), "_rocketchet_hook");
-  }
-
-  @DebugLog
-  @Override protected void onPageLoaded(WebView webview, String url) {
-    super.onPageLoaded(webview, url);
-
-
-    if (url.contains(hostname) && url.contains("_oauth/twitter?close")) {
-      final String jsHookUrl = "javascript:"
-          + "window._rocketchet_hook.handleConfig(document.getElementById('config').innerText);";
-      webview.loadUrl(jsHookUrl);
-    }
-  }
-
-  private interface JSInterfaceCallback {
-    void hanldeResult(@Nullable JSONObject result);
-  }
-
-  private static final class JSInterface {
-    private final JSInterfaceCallback jsInterfaceCallback;
-
-    JSInterface(JSInterfaceCallback callback) {
-      jsInterfaceCallback = callback;
-    }
-
-    @JavascriptInterface public void handleConfig(String config) {
-      try {
-        jsInterfaceCallback.hanldeResult(new JSONObject(config));
-      } catch (Exception exception) {
-        jsInterfaceCallback.hanldeResult(null);
-      }
-    }
-  }
-
-  private void handleOAuthCallback(final String credentialToken, final String credentialSecret) {
-    new MethodCallHelper(getContext(), serverConfigId)
-        .loginWithOAuth(credentialToken, credentialSecret)
-        .continueWith(new LogcatIfError());
-  }
-
-  private void onOAuthCompleted() {
-
   }
 }
