58177d52dc8ef88ac93df9e385d3635b3c6709b6
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2016-12-09 15:22:58 +0900

    fix findbugs issue.

--- app/src/main/java/chat/rocket/android/layouthelper/oauth/OAuthProviderInfo.java
+++ app/src/main/java/chat/rocket/android/layouthelper/oauth/OAuthProviderInfo.java
@@ -7,20 +7,28 @@ import chat.rocket.android.fragment.oauth.GitHubOAuthFragment;
 import chat.rocket.android.fragment.oauth.GoogleOAuthFragment;
 import chat.rocket.android.fragment.oauth.TwitterOAuthFragment;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
+/**
+ * View model for OAuth login button.
+ */
 public class OAuthProviderInfo {
   public String serviceName;
   public int buttonId;
   public Class<? extends AbstractOAuthFragment> fragmentClass;
 
-  public OAuthProviderInfo(String serviceName, int buttonId,
+  /**
+   * Constructor with required parameters.
+   */
+  private OAuthProviderInfo(String serviceName, int buttonId,
       Class<? extends AbstractOAuthFragment> fragmentClass) {
     this.serviceName = serviceName;
     this.buttonId = buttonId;
     this.fragmentClass = fragmentClass;
   }
 
-  public static final ArrayList<OAuthProviderInfo> LIST = new ArrayList<OAuthProviderInfo>() {
+  private static final ArrayList<OAuthProviderInfo> _LIST = new ArrayList<OAuthProviderInfo>() {
     {
       add(new OAuthProviderInfo(
           "twitter", R.id.btn_login_with_twitter, TwitterOAuthFragment.class));
@@ -32,4 +40,6 @@ public class OAuthProviderInfo {
           "facebook", R.id.btn_login_with_facebook, FacebookOAuthFragment.class));
     }
   };
+  
+  public static final List<OAuthProviderInfo> LIST = Collections.unmodifiableList(_LIST);
 }
