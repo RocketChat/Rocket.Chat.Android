c69e62f5e812baa08da1c0dd594ed0ac7d1c0e11
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2016-12-11 13:36:47 +0900

    implement notification click handler

--- app/src/main/java/chat/rocket/android/notification/StreamNotifyUserNotifier.java
+++ app/src/main/java/chat/rocket/android/notification/StreamNotifyUserNotifier.java
@@ -1,14 +1,20 @@
 package chat.rocket.android.notification;
 
 import android.app.Notification;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v4.content.ContextCompat;
 import bolts.Task;
 import chat.rocket.android.R;
+import chat.rocket.android.activity.MainActivity;
 import chat.rocket.android.helper.Avatar;
+import chat.rocket.android.model.ServerConfig;
+import chat.rocket.android.realm_helper.RealmStore;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 /**
@@ -71,12 +77,28 @@ public class StreamNotifyUserNotifier implements Notifier {
   }
 
   private Notification generateNotification(Bitmap largeIcon) {
+    Intent intent = new Intent(context, MainActivity.class);
+    intent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    ServerConfig config = RealmStore.getDefault().executeTransactionForRead(realm ->
+      realm.where(ServerConfig.class).equalTo("hostname", hostname).findFirst());
+    if (config != null) {
+      intent.putExtra("serverConfigId", config.getServerConfigId());
+      try {
+        intent.putExtra("roomId", payload.getString("rid"));
+      } catch (JSONException exception) {
+      }
+    }
+    PendingIntent pendingIntent = PendingIntent.getActivity(context.getApplicationContext(),
+        (int) (System.currentTimeMillis() % Integer.MAX_VALUE),
+        intent, PendingIntent.FLAG_ONE_SHOT);
+
     NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
         .setContentTitle(title)
         .setContentText(text)
         .setAutoCancel(true)
         .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
-        .setSmallIcon(R.drawable.rocket_chat_notification_24dp);
+        .setSmallIcon(R.drawable.rocket_chat_notification_24dp)
+        .setContentIntent(pendingIntent);
     if (largeIcon != null) {
       builder.setLargeIcon(largeIcon);
     }
