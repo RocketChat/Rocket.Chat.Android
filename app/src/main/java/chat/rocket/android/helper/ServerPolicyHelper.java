ec6255f5327d4daa5df496a6ca00257d642ebfff
Tiago Cunha <tiagocunha@gmail.com>
2017-01-02 17:36:04 -0200

    Unused import

--- app/src/main/java/chat/rocket/android/helper/ServerPolicyHelper.java
+++ app/src/main/java/chat/rocket/android/helper/ServerPolicyHelper.java
@@ -5,7 +5,6 @@ import android.support.annotation.NonNull;
 import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.regex.Pattern;
 import okhttp3.Call;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
