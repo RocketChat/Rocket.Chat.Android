23ea6c57c7a9e43715dc6fae155571a72186585c
Tiago Cunha <tiagocunha@gmail.com>
2017-01-09 19:03:56 -0200

    Changing to custom toolbar

--- app/src/main/java/chat/rocket/android/fragment/chatroom/AbstractChatRoomFragment.java
+++ app/src/main/java/chat/rocket/android/fragment/chatroom/AbstractChatRoomFragment.java
@@ -1,64 +1,50 @@
 package chat.rocket.android.fragment.chatroom;
 
-import android.graphics.Color;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.annotation.DrawableRes;
 import android.support.annotation.Nullable;
 import android.support.annotation.StringRes;
-import android.support.graphics.drawable.VectorDrawableCompat;
-import android.support.v4.graphics.drawable.DrawableCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import chat.rocket.android.R;
 import chat.rocket.android.fragment.AbstractFragment;
+import chat.rocket.android.widget.CustomToolbar;
 
 abstract class AbstractChatRoomFragment extends AbstractFragment {
 
-  private static final String TABLET = "tablet";
-
-  private TextView toolbarTitle;
+  private CustomToolbar customToolbar;
 
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container,
                            @Nullable Bundle savedInstanceState) {
-    toolbarTitle = (TextView) getActivity().findViewById(R.id.toolbar_title);
+    customToolbar = (CustomToolbar) getActivity().findViewById(R.id.activity_main_toolbar);
     return super.onCreateView(inflater, container, savedInstanceState);
   }
 
   protected void setTitleText(@StringRes int stringResId) {
-    if (toolbarTitle == null) {
+    if (customToolbar == null) {
       return;
     }
 
-    toolbarTitle.setText(stringResId);
+    customToolbar.setTitle(stringResId);
   }
 
   protected void setTitleText(CharSequence title) {
-    if (toolbarTitle == null) {
+    if (customToolbar == null) {
       return;
     }
 
-    toolbarTitle.setText(title);
+    customToolbar.setTitle(title);
   }
 
   protected void setTitleDrawableLeft(@DrawableRes int drawableResId) {
-    if (toolbarTitle == null) {
+    if (customToolbar == null) {
       return;
     }
 
-    Drawable drawable = drawableResId > 0
-        ? VectorDrawableCompat.create(getResources(), drawableResId, null)
-        : null;
-
-    if (drawable != null && TABLET.equals(toolbarTitle.getTag())) {
-      DrawableCompat.setTint(drawable, Color.WHITE);
-    }
-
-    toolbarTitle.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null);
+    customToolbar.setTitleDrawableLeft(drawableResId);
   }
 }
