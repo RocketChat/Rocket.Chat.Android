7ff619a2df8f8545b8fe243ff56953dbf4d1b5cf
Tiago Cunha <tiagocunha@gmail.com>
2017-01-16 14:36:05 -0200

    Implemented equals and hashCode
    Using IDE helpers

--- app/src/main/java/chat/rocket/android/model/ddp/Email.java
+++ app/src/main/java/chat/rocket/android/model/ddp/Email.java
@@ -9,4 +9,29 @@ import io.realm.annotations.PrimaryKey;
 public class Email extends RealmObject {
   @PrimaryKey private String address;
   private boolean verified;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    Email email = (Email) o;
+
+    if (verified != email.verified) {
+      return false;
+    }
+    return address != null ? address.equals(email.address) : email.address == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = address != null ? address.hashCode() : 0;
+    result = 31 * result + (verified ? 1 : 0);
+    return result;
+  }
 }
--- app/src/main/java/chat/rocket/android/model/ddp/Preferences.java
+++ app/src/main/java/chat/rocket/android/model/ddp/Preferences.java
@@ -84,4 +84,86 @@ public class Preferences extends RealmObject {
   public boolean isHideFlexTab() {
     return hideFlexTab;
   }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+
+    Preferences that = (Preferences) o;
+
+    if (newRoomNotification != that.newRoomNotification) {
+      return false;
+    }
+    if (newMessageNotification != that.newMessageNotification) {
+      return false;
+    }
+    if (useEmojis != that.useEmojis) {
+      return false;
+    }
+    if (convertAsciiEmoji != that.convertAsciiEmoji) {
+      return false;
+    }
+    if (saveMobileBandwidth != that.saveMobileBandwidth) {
+      return false;
+    }
+    if (collapseMediaByDefault != that.collapseMediaByDefault) {
+      return false;
+    }
+    if (unreadRoomsMode != that.unreadRoomsMode) {
+      return false;
+    }
+    if (autoImageLoad != that.autoImageLoad) {
+      return false;
+    }
+    if (unreadAlert != that.unreadAlert) {
+      return false;
+    }
+    if (desktopNotificationDuration != that.desktopNotificationDuration) {
+      return false;
+    }
+    if (viewMode != that.viewMode) {
+      return false;
+    }
+    if (hideUsernames != that.hideUsernames) {
+      return false;
+    }
+    if (hideAvatars != that.hideAvatars) {
+      return false;
+    }
+    if (hideFlexTab != that.hideFlexTab) {
+      return false;
+    }
+    if (id != null ? !id.equals(that.id) : that.id != null) {
+      return false;
+    }
+    return emailNotificationMode != null ? emailNotificationMode.equals(that.emailNotificationMode)
+        : that.emailNotificationMode == null;
+
+  }
+
+  @Override
+  public int hashCode() {
+    int result = id != null ? id.hashCode() : 0;
+    result = 31 * result + (newRoomNotification ? 1 : 0);
+    result = 31 * result + (newMessageNotification ? 1 : 0);
+    result = 31 * result + (useEmojis ? 1 : 0);
+    result = 31 * result + (convertAsciiEmoji ? 1 : 0);
+    result = 31 * result + (saveMobileBandwidth ? 1 : 0);
+    result = 31 * result + (collapseMediaByDefault ? 1 : 0);
+    result = 31 * result + (unreadRoomsMode ? 1 : 0);
+    result = 31 * result + (autoImageLoad ? 1 : 0);
+    result = 31 * result + (emailNotificationMode != null ? emailNotificationMode.hashCode() : 0);
+    result = 31 * result + (unreadAlert ? 1 : 0);
+    result = 31 * result + desktopNotificationDuration;
+    result = 31 * result + viewMode;
+    result = 31 * result + (hideUsernames ? 1 : 0);
+    result = 31 * result + (hideAvatars ? 1 : 0);
+    result = 31 * result + (hideFlexTab ? 1 : 0);
+    return result;
+  }
 }
