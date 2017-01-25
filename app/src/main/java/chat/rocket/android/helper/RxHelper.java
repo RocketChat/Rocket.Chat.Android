609f9bdaba73a4c60529dbfceeee450fcf5105af
Yusuke Iwaki <yusuke.iwaki@crowdworks.co.jp>
2017-01-25 21:58:42 +0900

    rename variable name and add comment.

--- app/src/main/java/chat/rocket/android/helper/RxHelper.java
+++ app/src/main/java/chat/rocket/android/helper/RxHelper.java
@@ -10,7 +10,9 @@ import rx.functions.Func1;
 public class RxHelper {
   public static Func1<Observable<? extends Throwable>, Observable<?>> exponentialBackoff(
       int maxRetryCount, long base, TimeUnit unit) {
-    return errors -> errors
+
+    // ref: https://github.com/ReactiveX/RxJava/blob/a8ba158839b67246a742b6f1531995ffd7545c08/src/main/java/io/reactivex/Observable.java#L9601
+    return attempts -> attempts
         .zipWith(Observable.range(0, maxRetryCount), (error, retryCount) -> retryCount)
         .flatMap(retryCount -> Observable.timer(base * (long) Math.pow(2, retryCount), unit));
   }
