ef6378bafd99a543e9c6c36497e4a3e84776593f
Tiago Cunha <tiagocunha@gmail.com>
2017-01-27 15:41:24 -0200

    Won't set an empty cookie

--- app/src/main/java/chat/rocket/android/api/rest/CookieInterceptor.java
+++ app/src/main/java/chat/rocket/android/api/rest/CookieInterceptor.java
@@ -1,6 +1,7 @@
 package chat.rocket.android.api.rest;
 
 import java.io.IOException;
+import chat.rocket.android.helper.TextUtils;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
@@ -16,10 +17,14 @@ public class CookieInterceptor implements Interceptor {
   @Override
   public Response intercept(Chain chain) throws IOException {
     if (chain.request().url().host().equals(cookieProvider.getHostname())) {
-      Request newRequest = chain.request().newBuilder()
-          .header("Cookie", cookieProvider.getCookie())
-          .build();
-      return chain.proceed(newRequest);
+      final String cookie = cookieProvider.getCookie();
+
+      if (!TextUtils.isEmpty(cookie)) {
+        Request newRequest = chain.request().newBuilder()
+            .header("Cookie", cookie)
+            .build();
+        return chain.proceed(newRequest);
+      }
     }
 
     return chain.proceed(chain.request());
