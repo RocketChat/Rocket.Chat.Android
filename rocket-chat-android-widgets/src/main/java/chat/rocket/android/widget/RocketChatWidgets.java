8b343180911241a7c978f0cb5d0316813a7ca246
Tiago Cunha <tiagocunha@gmail.com>
2017-01-27 15:17:44 -0200

    Removed unused imports

--- rocket-chat-android-widgets/src/main/java/chat/rocket/android/widget/RocketChatWidgets.java
+++ rocket-chat-android-widgets/src/main/java/chat/rocket/android/widget/RocketChatWidgets.java
@@ -1,13 +1,8 @@
 package chat.rocket.android.widget;
 
 import android.content.Context;
-import com.facebook.drawee.backends.pipeline.DraweeConfig;
-import com.facebook.drawee.backends.pipeline.Fresco;
-import com.facebook.imagepipeline.backends.okhttp3.OkHttpImagePipelineConfigFactory;
-import com.facebook.imagepipeline.core.ImagePipelineConfig;
 
 import chat.rocket.android.widget.fresco.FrescoConfiguration;
-import chat.rocket.android.widget.fresco.ImageFormatConfigurator;
 import okhttp3.OkHttpClient;
 
 public class RocketChatWidgets {
