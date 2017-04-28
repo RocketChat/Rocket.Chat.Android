b920ef077c04ec43b3f0f2e6fe0357fd6ef8bc6c
Tiago Cunha <tiagocunha@gmail.com>
2017-04-28 11:28:29 -0300

    Realm repositories

--- rocket-chat-core/src/main/java/chat/rocket/core/repositories/PermissionRepository.java
+++ rocket-chat-core/src/main/java/chat/rocket/core/repositories/PermissionRepository.java
@@ -3,13 +3,9 @@ package chat.rocket.core.repositories;
 import com.fernandocejas.arrow.optional.Optional;
 import io.reactivex.Single;
 
-import java.util.List;
 import chat.rocket.core.models.Permission;
-import chat.rocket.core.models.Role;
 
 public interface PermissionRepository {
-
-  Single<List<Permission>> getFor(Role role);
 
   Single<Optional<Permission>> getById(String id);
 }
