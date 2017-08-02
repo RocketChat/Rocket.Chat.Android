f35ab1419bdafa8dfc6a9c64be67127e489216bb
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-08-02 18:53:52 -0300

    Delete image_svg_user_avatar/imageViewSvgUserAvatar

--- app/src/main/java/chat/rocket/android/layouthelper/chatroom/AbstractMessageViewHolder.java
+++ app/src/main/java/chat/rocket/android/layouthelper/chatroom/AbstractMessageViewHolder.java
@@ -14,7 +14,6 @@ import chat.rocket.core.SyncState;
 public abstract class AbstractMessageViewHolder extends ModelViewHolder<PairedMessage> {
   protected final RocketChatAvatar avatar;
   protected final ImageView userNotFoundAvatarImageView;
-  protected final ImageView imageViewSvgUserAvatar;
   protected final ImageView errorImageView;
   protected final TextView username;
   protected final TextView subUsername;
@@ -32,7 +31,6 @@ public abstract class AbstractMessageViewHolder extends ModelViewHolder<PairedMe
     super(itemView);
     avatar = itemView.findViewById(R.id.user_avatar);
     userNotFoundAvatarImageView = itemView.findViewById(R.id.userNotFoundAvatarImageView);
-    imageViewSvgUserAvatar = itemView.findViewById(R.id.image_svg_user_avatar);
     errorImageView = itemView.findViewById(R.id.errorImageView);
     username = itemView.findViewById(R.id.username);
     subUsername = itemView.findViewById(R.id.sub_username);
