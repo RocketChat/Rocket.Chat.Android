55e9533651cdee5ab87c25bf1d6cbf72347e3e16
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-08-22 21:08:12 -0300

    Update UserRepository class.
    From Java to Kotlin. Also delete the SortDirection attribute (avoid conditionals checks).

---
+++ rocket-chat-core/src/main/java/chat/rocket/core/repositories/UserRepository.kt
@@ -0,0 +1,14 @@
+package chat.rocket.core.repositories
+
+import com.hadisatrio.optional.Optional
+import io.reactivex.Flowable
+import chat.rocket.core.models.User
+
+interface UserRepository {
+
+    fun getCurrent(): Flowable<Optional<User>>
+
+    fun getByUsername(username: String): Flowable<Optional<User>>
+
+    fun getSortedLikeName(name: String, limit: Int): Flowable<List<User>>
+}
