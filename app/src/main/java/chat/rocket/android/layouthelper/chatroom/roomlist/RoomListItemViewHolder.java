60132da449b060309047461b5459dc682005cd51
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-08-29 19:16:25 -0300

    Update RoomListItemViewHolder.java

--- app/src/main/java/chat/rocket/android/layouthelper/chatroom/roomlist/RoomListItemViewHolder.java
+++ app/src/main/java/chat/rocket/android/layouthelper/chatroom/roomlist/RoomListItemViewHolder.java
@@ -5,42 +5,91 @@ import android.support.v7.widget.RecyclerView;
 import chat.rocket.android.widget.internal.RoomListItemView;
 import chat.rocket.core.models.Room;
 import chat.rocket.core.models.Spotlight;
+import chat.rocket.core.models.User;
 
 public class RoomListItemViewHolder extends RecyclerView.ViewHolder {
-  public RoomListItemViewHolder(RoomListItemView itemView,
-                                RoomListAdapter.OnItemClickListener listener) {
+  private RoomListItemView itemView;
+
+  public RoomListItemViewHolder(RoomListItemView itemView, RoomListAdapter.OnItemClickListener listener) {
     super(itemView);
 
-    itemView.setOnClickListener(view -> {
-      if (listener != null) {
-        Object tag = view.getTag();
+    this.itemView = itemView;
 
-        if (tag instanceof Room) {
-          listener.onItemClick((Room) view.getTag());
-        } else if (tag instanceof Spotlight) {
-          listener.onItemClick((Spotlight) view.getTag());
-        }
+    itemView.setOnClickListener(view -> {
+      Object object = view.getTag();
+      if (object instanceof Room) {
+        listener.onItemClick((Room)object);
+      } else if (object instanceof Spotlight) {
+        listener.onItemClick((Spotlight)object);
       }
     });
   }
 
+
   public void bind(Room room) {
-    ((RoomListItemView) itemView)
+    itemView
         .setRoomId(room.getRoomId())
         .setRoomName(room.getName())
-        .setRoomType(room.getType())
         .setAlert(room.isAlert())
         .setUnreadCount(room.getUnread())
         .setTag(room);
+
+      showRoomIcon(room.getType());
   }
 
   public void bind(Spotlight spotlight) {
-    ((RoomListItemView) itemView)
+    itemView
         .setRoomId(spotlight.getId())
         .setRoomName(spotlight.getName())
-        .setRoomType(spotlight.getType())
         .setAlert(false)
         .setUnreadCount(0)
         .setTag(spotlight);
+
+    showRoomIcon(spotlight.getType());
+  }
+
+  /**
+   * Only shows the room icon if it is a PRIVATE CHANNEL or PUBLIC CHANNEL, otherwise you should use {@link #bind(User)} to show the correct icon.
+   * @param roomType The type of Room.
+   * @see Room
+   */
+  private void showRoomIcon(String roomType) {
+    if(!roomType.equals(Room.TYPE_DIRECT_MESSAGE)) {
+      switch (roomType) {
+        case Room.TYPE_CHANNEL:
+          itemView.showPublicChannelIcon();
+          break;
+        case Room.TYPE_PRIVATE:
+          itemView.showPrivateChannelIcon();
+          break;
+      }
+    }
+  }
+
+  /**
+   * Shows the user status icon.
+   * @param user The user to show its status.
+   * @see User
+   */
+  public void bind(User user) {
+    String userStatus = user.getStatus();
+    if (userStatus == null) {
+      itemView.showOfflineUserStatusIcon();
+    } else {
+      switch (user.getStatus()) {
+        case User.STATUS_ONLINE:
+          itemView.showOnlineUserStatusIcon();
+          break;
+        case User.STATUS_BUSY:
+          itemView.showBusyUserStatusIcon();
+          break;
+        case User.STATUS_AWAY:
+          itemView.showAwayUserStatusIcon();
+          break;
+        default:
+          itemView.showOfflineUserStatusIcon();
+          break;
+      }
+    }
   }
 }
