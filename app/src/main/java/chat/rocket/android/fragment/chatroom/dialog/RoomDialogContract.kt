7ebf655c655edd7040509eec4ed982f5c305a4c1
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-09-15 19:42:45 -0300

    Update RoomDialogContract.kt

--- app/src/main/java/chat/rocket/android/fragment/chatroom/dialog/RoomDialogContract.kt
+++ app/src/main/java/chat/rocket/android/fragment/chatroom/dialog/RoomDialogContract.kt
@@ -1,9 +1,11 @@
 package chat.rocket.android.fragment.chatroom.dialog
 
+import chat.rocket.core.models.Message
+
 interface RoomDialogContract {
 
     interface View {
-        fun showPinnedMessages()
+        fun showPinnedMessages(dataSet: ArrayList<Message>)
         fun showFavoriteMessages()
         fun showFileList(dataSet: ArrayList<String>)
         fun showMemberList(dataSet: ArrayList<String>)
