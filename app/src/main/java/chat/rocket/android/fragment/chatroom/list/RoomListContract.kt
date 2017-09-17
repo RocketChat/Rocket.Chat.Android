d4f5bd104bd9a9dc6cb268be7d1a6f736683bfbb
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-09-17 17:44:53 -0300

    Add RoomListContract.kt

---
+++ app/src/main/java/chat/rocket/android/fragment/chatroom/list/RoomListContract.kt
@@ -0,0 +1,107 @@
+package chat.rocket.android.fragment.chatroom.list
+
+import chat.rocket.core.models.Message
+import chat.rocket.core.models.User
+
+interface RoomListContract {
+
+    interface View {
+
+        /**
+         * Shows a pinned message list of a room.
+         *
+         * @param dataSet The pinned message data set to show.
+         */
+        fun showPinnedMessages(dataSet: ArrayList<Message>)
+
+        /**
+         * Shows a favorite message list of a room.
+         *
+         * @param dataSet The favorite message data set to show.
+         */
+        fun showFavoriteMessages(dataSet: ArrayList<Message>)
+
+        /**
+         * Shows a file list of a room.
+         *
+         * @param dataSet The file data set to show.
+         */
+        fun showFileList(dataSet: ArrayList<String>)
+
+        /**
+         * Shows a list of members of a room.
+         *
+         * @param dataSet The member data set to show.
+         */
+        fun showMemberList(dataSet: ArrayList<User>)
+
+        /**
+         * Shows a message (e.g. An error or successful message after a request).
+         *
+         * @param message The message to show.
+         */
+        fun showMessage(message: String)
+    }
+
+    interface Presenter {
+        /**
+         * Requests the pinned messages of a room.
+         *
+         * @param roomId The room ID to process the request.
+         * @param roomType The room type to process the request.
+         * @param hostname The server hostname to process the request.
+         * @param token The token to process the request.
+         * @param userId The user ID to process the request.
+         */
+        fun requestPinnedMessages(roomId: String,
+                       roomType: String,
+                       hostname: String,
+                       token: String,
+                       userId: String)
+
+        /**
+         * Requests the favorite messages of a room.
+         *
+         * @param roomId The room ID to process the request.
+         * @param roomType The room type to process the request.
+         * @param hostname The server hostname to process the request.
+         * @param token The token to process the request.
+         * @param userId The user ID to process the request.
+         */
+        fun requestFavoriteMessages(roomId: String,
+                                  roomType: String,
+                                  hostname: String,
+                                  token: String,
+                                  userId: String)
+
+        /**
+         * Requests the file list of a room.
+         *
+         * @param roomId The room ID to process the request.
+         * @param roomType The room type to process the request.
+         * @param hostname The server hostname to process the request.
+         * @param token The token to process the request.
+         * @param userId The user ID to process the request.
+         */
+        fun requestFileList(roomId: String,
+                                  roomType: String,
+                                  hostname: String,
+                                  token: String,
+                                  userId: String)
+
+        /**
+         * Requests the member list of a room.
+         *
+         * @param roomId The room ID to process the request.
+         * @param roomType The room type to process the request.
+         * @param hostname The server hostname to process the request.
+         * @param token The token to process the request.
+         * @param userId The user ID to process the request.
+         */
+        fun requestMemberList(roomId: String,
+                                  roomType: String,
+                                  hostname: String,
+                                  token: String,
+                                  userId: String)
+    }
+}
