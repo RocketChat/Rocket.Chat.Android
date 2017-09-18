4769b3915dde59fb38c2c17eed66bff2256f6ebc
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-09-18 12:25:15 -0300

    Add activity/room/RoomActivity.kt

---
+++ app/src/main/java/chat/rocket/android/activity/room/RoomActivity.kt
@@ -0,0 +1,43 @@
+package chat.rocket.android.activity.room
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
+import chat.rocket.android.R
+import chat.rocket.android.fragment.chatroom.list.RoomListFragment
+import kotlinx.android.synthetic.main.activity_room.*
+
+class RoomActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room)
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setDisplayShowHomeEnabled(true)
+
+        val extras = intent.extras
+        val roomListFragment = RoomListFragment.newInstance(extras.getInt("actionId"),
+                extras.getString("roomId"),
+                extras.getString("roomType"),
+                extras.getString("hostname"),
+                extras.getString("token"),
+                extras.getString("userId"))
+
+        addFragment(roomListFragment, "roomListFragment")
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
+    private fun addFragment(fragment: Fragment, tag: String) {
+        supportFragmentManager
+                .beginTransaction()
+                .add(R.id.fragment_container, fragment, tag)
+                .commit()
+    }
+}
