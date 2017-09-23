41bae5a7e4762e0a35f256f00c557d6e73acf5e9
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-09-22 23:08:34 -0300

    Update RoomListContract.kt

--- app/src/main/java/chat/rocket/android/fragment/chatroom/list/RoomListContract.kt
+++ app/src/main/java/chat/rocket/android/fragment/chatroom/list/RoomListContract.kt
@@ -3,6 +3,9 @@ package chat.rocket.android.fragment.chatroom.list
 import chat.rocket.core.models.Message
 import chat.rocket.core.models.User
 
+/**
+ * Created by Filipe de Lima Brito (filipedelimabrito@gmail.com) on 9/22/17.
+ */
 interface RoomListContract {
 
     interface View {
@@ -11,22 +14,25 @@ interface RoomListContract {
          * Shows a pinned message list of a room.
          *
          * @param dataSet The pinned message data set to show.
+         * @param total The total number of pinned messages.
          */
-        fun showPinnedMessages(dataSet: ArrayList<Message>)
+        fun showPinnedMessages(dataSet: ArrayList<Message>, total: String)
 
         /**
          * Shows a favorite message list of a room.
          *
          * @param dataSet The favorite message data set to show.
+         * @param total The total number of favorite messages.
          */
-        fun showFavoriteMessages(dataSet: ArrayList<Message>)
+        fun showFavoriteMessages(dataSet: ArrayList<Message>, total: String)
 
         /**
          * Shows a file list of a room.
          *
          * @param dataSet The file data set to show.
+         * @param total The total number of files.
          */
-        fun showFileList(dataSet: ArrayList<String>)
+        fun showFileList(dataSet: ArrayList<String>, total: String)
 
         /**
          * Shows a list of members of a room.
@@ -42,6 +48,13 @@ interface RoomListContract {
          * @param message The message to show.
          */
         fun showMessage(message: String)
+
+        /**
+         * Shows a waiting view whenever a (long) process is taken.
+         *
+         * @param shouldShow The Boolean value that indicates whether the view should be showed.
+         */
+        fun showWaitingView(shouldShow: Boolean)
     }
 
     interface Presenter {
@@ -53,14 +66,14 @@ interface RoomListContract {
          * @param hostname The server hostname to process the request.
          * @param token The token to process the request.
          * @param userId The user ID to process the request.
-         * @param offset The offset to start to process the request.
+         * @param offset The offset to process the request.
          */
         fun requestPinnedMessages(roomId: String,
                                   roomType: String,
                                   hostname: String,
                                   token: String,
                                   userId: String,
-                                  offset: String)
+                                  offset: Int)
 
         /**
          * Requests the favorite messages of a room.
@@ -70,14 +83,14 @@ interface RoomListContract {
          * @param hostname The server hostname to process the request.
          * @param token The token to process the request.
          * @param userId The user ID to process the request.
-         * @param offset The offset to start to process the request.
+         * @param offset The offset to process the request.
          */
         fun requestFavoriteMessages(roomId: String,
                                     roomType: String,
                                     hostname: String,
                                     token: String,
                                     userId: String,
-                                    offset: String)
+                                    offset: Int)
 
         /**
          * Requests the file list of a room.
@@ -87,14 +100,14 @@ interface RoomListContract {
          * @param hostname The server hostname to process the request.
          * @param token The token to process the request.
          * @param userId The user ID to process the request.
-         * @param offset The offset to start to process the request.
+         * @param offset The offset to process the request.
          */
         fun requestFileList(roomId: String,
                             roomType: String,
                             hostname: String,
                             token: String,
                             userId: String,
-                            offset: String)
+                            offset: Int)
 
         /**
          * Requests the member list of a room.
@@ -104,13 +117,13 @@ interface RoomListContract {
          * @param hostname The server hostname to process the request.
          * @param token The token to process the request.
          * @param userId The user ID to process the request.
-         * @param offset The offset to start to process the request.
+         * @param offset The offset to process the request.
          */
         fun requestMemberList(roomId: String,
                               roomType: String,
                               hostname: String,
                               token: String,
                               userId: String,
-                              offset: String)
+                              offset: Int)
     }
 }
