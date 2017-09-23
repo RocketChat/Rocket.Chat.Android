526df665932f6199c3fab7cad6d13d1e544cdf5a
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-09-22 23:10:53 -0300

    Update RoomFileListAdapter.kt

--- app/src/main/java/chat/rocket/android/layouthelper/chatroom/list/RoomFileListAdapter.kt
+++ app/src/main/java/chat/rocket/android/layouthelper/chatroom/list/RoomFileListAdapter.kt
@@ -8,7 +8,10 @@ import chat.rocket.android.R
 import chat.rocket.android.widget.message.RocketChatMessageLayout
 import kotlinx.android.synthetic.main.item_room_file.view.*
 
-class RoomFileListAdapter(private val dataSet: List<String>) : RecyclerView.Adapter<RoomFileListAdapter.ViewHolder>() {
+/**
+ * Created by Filipe de Lima Brito (filipedelimabrito@gmail.com) on 9/22/17.
+ */
+class RoomFileListAdapter(private var dataSet: List<String>) : RecyclerView.Adapter<RoomFileListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_room_file, parent, false)
@@ -20,6 +23,11 @@ class RoomFileListAdapter(private val dataSet: List<String>) : RecyclerView.Adap
     }
 
     override fun getItemCount(): Int = dataSet.size
+
+    fun setDataSet(dataSet: List<String>) {
+        this.dataSet = dataSet
+        notifyDataSetChanged()
+    }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val fileNameLink : RocketChatMessageLayout = itemView.fileLink
