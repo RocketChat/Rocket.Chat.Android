d6d36e1ce8cc4bcb5d5c8319c5df0ab505c6ab31
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-09-25 17:14:12 -0300

    Update RoomMemberListAdapter.kt

--- app/src/main/java/chat/rocket/android/layouthelper/chatroom/list/RoomMemberListAdapter.kt
+++ app/src/main/java/chat/rocket/android/layouthelper/chatroom/list/RoomMemberListAdapter.kt
@@ -54,9 +54,10 @@ class RoomMemberListAdapter(private var dataSet: List<User>, private val hostnam
 
     override fun getItemCount(): Int = dataSet.size
 
-    fun setDataSet(dataSet: List<User>) {
-        this.dataSet = dataSet
-        notifyDataSetChanged()
+    fun addDataSet(dataSet: List<User>) {
+        val previousDataSetSize = this.dataSet.size
+        this.dataSet += dataSet
+        notifyItemRangeInserted(previousDataSetSize, dataSet.size)
     }
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
