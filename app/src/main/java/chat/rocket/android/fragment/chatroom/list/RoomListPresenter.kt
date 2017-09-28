d675b1e8b3d3897e72900d95802a8ca395f4b23f
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-09-28 17:50:05 -0300

    Update RoomListPresenter.kt

--- app/src/main/java/chat/rocket/android/fragment/chatroom/list/RoomListPresenter.kt
+++ app/src/main/java/chat/rocket/android/fragment/chatroom/list/RoomListPresenter.kt
@@ -2,6 +2,7 @@ package chat.rocket.android.fragment.chatroom.list
 
 import android.content.Context
 import android.os.Handler
+import android.util.Log
 import chat.rocket.android.R
 import chat.rocket.android.api.rest.RestApiHelper
 import chat.rocket.android.helper.OkHttpHelper
@@ -11,6 +12,7 @@ import chat.rocket.core.models.User
 import okhttp3.Call
 import okhttp3.Callback
 import okhttp3.Response
+import org.json.JSONException
 import org.json.JSONObject
 import java.io.IOException
 import java.sql.Timestamp
@@ -147,72 +149,80 @@ class RoomListPresenter(val context: Context, val view: RoomListContract.View) :
     }
 
     private fun handleMessagesJson(json: String, isPinnedMessage: Boolean) {
-        val jSONObject = JSONObject(json)
-        val messagesJSONArray = jSONObject.getJSONArray("messages")
+        try {
+            val jSONObject = JSONObject(json)
+            val messagesJSONArray = jSONObject.getJSONArray("messages")
 
-        val total = messagesJSONArray.length()
-        val dataSet = ArrayList<Message>(total)
-        (0 until total).mapTo(dataSet) {
-            val messageJsonObject = messagesJSONArray.getJSONObject(it)
-            val userJsonObject = messageJsonObject.getJSONObject("u")
+            val total = messagesJSONArray.length()
+            val dataSet = ArrayList<Message>(total)
+            (0 until total).mapTo(dataSet) {
+                val messageJsonObject = messagesJSONArray.getJSONObject(it)
+                val userJsonObject = messageJsonObject.getJSONObject("u")
 
-            val timestampString = messageJsonObject.optString("ts")
-            val timestamp = if (timestampString.isBlank()) {
-                0
-            } else {
-                Timestamp.valueOf(timestampString.replace("T", " ").replace("Z", "")).time
-            }
-
-            val editedAtString = messageJsonObject.optString("_updatedAt")
-            val editedAt = if (editedAtString.isBlank()) {
-                0
-            } else {
-                Timestamp.valueOf(editedAtString.replace("T", " ").replace("Z", "")).time
-            }
-
-            Message.builder()
-                    .setId(messageJsonObject.optString("_id"))
-                    .setRoomId(messageJsonObject.optString("rid"))
-                    .setMessage(messageJsonObject.optString("msg"))
-                    .setUser(getUserFromJsonObject(userJsonObject))
-                    .setTimestamp(timestamp)
-                    .setEditedAt(editedAt)
-                    .setGroupable(messageJsonObject.optBoolean("groupable"))
-                    .setSyncState(SyncState.SYNCED)
-                    .build()
-        }
-
-        if (dataSet.isEmpty() && !hasItem) {
-            showEmptyViewMessage(context.getString(R.string.fragment_room_list_no_favorite_message_to_show))
-        } else {
-            if (dataSet.isNotEmpty()) {
-                hasItem = true
-                if (isPinnedMessage) {
-                    showPinnedMessageList(dataSet, jSONObject.optString("total"))
+                val timestampString = messageJsonObject.optString("ts")
+                val timestamp = if (timestampString.isBlank()) {
+                    0
                 } else {
-                    showFavoriteMessageList(dataSet, jSONObject.optString("total"))
+                    Timestamp.valueOf(timestampString.replace("T", " ").replace("Z", "")).time
+                }
+
+                val editedAtString = messageJsonObject.optString("_updatedAt")
+                val editedAt = if (editedAtString.isBlank()) {
+                    0
+                } else {
+                    Timestamp.valueOf(editedAtString.replace("T", " ").replace("Z", "")).time
+                }
+
+                Message.builder()
+                        .setId(messageJsonObject.optString("_id"))
+                        .setRoomId(messageJsonObject.optString("rid"))
+                        .setMessage(messageJsonObject.optString("msg"))
+                        .setUser(getUserFromJsonObject(userJsonObject))
+                        .setTimestamp(timestamp)
+                        .setEditedAt(editedAt)
+                        .setGroupable(messageJsonObject.optBoolean("groupable"))
+                        .setSyncState(SyncState.SYNCED)
+                        .build()
+            }
+
+            if (dataSet.isEmpty() && !hasItem) {
+                showEmptyViewMessage(context.getString(R.string.fragment_room_list_no_favorite_message_to_show))
+            } else {
+                if (dataSet.isNotEmpty()) {
+                    hasItem = true
+                    if (isPinnedMessage) {
+                        showPinnedMessageList(dataSet, jSONObject.optString("total"))
+                    } else {
+                        showFavoriteMessageList(dataSet, jSONObject.optString("total"))
+                    }
                 }
             }
+        } catch (exception: JSONException) {
+            showInvalidRequest()
         }
     }
 
     private fun handleMembersJson(json: String) {
-        val jsonObject = JSONObject(json)
-        val membersJsonArray = jsonObject.getJSONArray("members")
+        try {
+            val jsonObject = JSONObject(json)
+            val membersJsonArray = jsonObject.getJSONArray("members")
 
-        val total = membersJsonArray.length()
-        val dataSet = ArrayList<User>(total)
-        (0 until total).mapTo(dataSet) {
-            getUserFromJsonObject(membersJsonArray.getJSONObject(it))
-        }
-
-        if (dataSet.isEmpty() && !hasItem) {
-            showEmptyViewMessage(context.getString(R.string.fragment_room_list_no_member_list_to_show))
-        } else {
-            if (dataSet.isNotEmpty()) {
-                hasItem = true
-                showMemberList(dataSet, jsonObject.optString("total"))
+            val total = membersJsonArray.length()
+            val dataSet = ArrayList<User>(total)
+            (0 until total).mapTo(dataSet) {
+                getUserFromJsonObject(membersJsonArray.getJSONObject(it))
             }
+
+            if (dataSet.isEmpty() && !hasItem) {
+                showEmptyViewMessage(context.getString(R.string.fragment_room_list_no_member_list_to_show))
+            } else {
+                if (dataSet.isNotEmpty()) {
+                    hasItem = true
+                    showMemberList(dataSet, jsonObject.optString("total"))
+                }
+            }
+        }catch (exception: JSONException) {
+            showInvalidRequest()
         }
     }
 
@@ -244,6 +254,13 @@ class RoomListPresenter(val context: Context, val view: RoomListContract.View) :
         mainHandler.post {
             view.showWaitingView(false)
             view.showMemberList(dataSet, total)
+        }
+    }
+
+    private fun showInvalidRequest() {
+        mainHandler.post {
+            view.showWaitingView(false)
+            view.showMessage(context.getString(R.string.fragment_room_list_could_not_load_your_request, context.getString(R.string.make_sure_your_server_version_is_up_to_date)))
         }
     }
 
