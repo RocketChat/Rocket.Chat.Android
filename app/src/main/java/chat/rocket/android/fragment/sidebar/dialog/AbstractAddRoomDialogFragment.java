56fe897ea329de6d39e543c9526ceb3580e57064
HarveyDoom <chrishampsongtd@gmail.com>
2017-10-04 14:32:06 +1100

    Added Import Statement

--- app/src/main/java/chat/rocket/android/fragment/sidebar/dialog/AbstractAddRoomDialogFragment.java
+++ app/src/main/java/chat/rocket/android/fragment/sidebar/dialog/AbstractAddRoomDialogFragment.java
@@ -6,6 +6,7 @@ import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Toast;
 
 import com.trello.rxlifecycle2.components.support.RxAppCompatDialogFragment;
