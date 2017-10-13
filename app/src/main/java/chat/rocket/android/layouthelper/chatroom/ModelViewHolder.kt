5c639b99d3132261b38793e0ca47218b551b4d69
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-10-13 16:29:59 -0300

    Add ModelViewHolder.kt

---
+++ app/src/main/java/chat/rocket/android/layouthelper/chatroom/ModelViewHolder.kt
@@ -0,0 +1,9 @@
+package chat.rocket.android.layouthelper.chatroom
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+
+abstract class ModelViewHolder<in T>(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    abstract fun bind(model: T, autoLoadImages: Boolean)
+}
