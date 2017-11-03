05761d2f4e633d65a99d83eb21f5d0d18bf086d7
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-11-03 12:22:55 -0300

    Add new methods.

--- app/src/main/java/chat/rocket/android/app/DrawableHelper.kt
+++ app/src/main/java/chat/rocket/android/app/DrawableHelper.kt
@@ -22,6 +22,7 @@ object DrawableHelper {
      * Wraps an array of Drawable to be used for example for tinting.
      *
      * @param drawables The array of Drawable to wrap.
+     * @see wrapDrawable
      * @see tintDrawables
      */
     fun wrapDrawables(drawables: Array<Drawable>) {
@@ -31,6 +32,16 @@ object DrawableHelper {
     }
 
     /**
+     * Wraps the Drawable to be used for example for tinting.
+     *
+     * @param drawable The Drawable to wrap.
+     * @see wrapDrawables
+     * @see tintDrawable
+     */
+    fun wrapDrawable(drawable: Drawable) = DrawableCompat.wrap(drawable)
+
+
+    /**
      * Tints an array of Drawable.
      *
      * REMARK: you MUST always wrap the array of Drawable before tint it.
@@ -38,6 +49,7 @@ object DrawableHelper {
      * @param drawables The array of Drawable to tint.
      * @param context The context.
      * @param resId The resource id color to tint the Drawables.
+     * @see tintDrawable
      * @see wrapDrawables
      */
     fun tintDrawables(drawables: Array<Drawable>, context: Context, resId: Int) {
@@ -47,12 +59,26 @@ object DrawableHelper {
     }
 
     /**
+     * Tints a Drawable.
+     *
+     * REMARK: you MUST always wrap the Drawable before tint it.
+     *
+     * @param drawable The Drawable to tint.
+     * @param context The context.
+     * @param resId The resource id color to tint the Drawable.
+     * @see tintDrawables
+     * @see wrapDrawable
+     */
+    fun tintDrawable(drawable: Drawable, context: Context, resId: Int) = DrawableCompat.setTint(drawable, ContextCompat.getColor(context, resId))
+
+    /**
      * Compounds an array of Drawable (to appear to the left of the text) into an array of EditText.
      *
      * REMARK: the number of elements in both array of Drawable and EditText MUST be equal.
      *
      * @param editTexts The array of EditText.
      * @param drawables The array of Drawable.
+     * @see compoundDrawable
      */
     fun compoundDrawables(editTexts: Array<EditText>, drawables: Array<Drawable>) {
         if (editTexts.size != drawables.size) {
@@ -63,4 +89,13 @@ object DrawableHelper {
             }
         }
     }
+
+    /**
+     * Compounds a Drawable (to appear to the left of the text) into an EditText.
+     *
+     * @param editText The EditText.
+     * @param drawable The Drawable.
+     * @see compoundDrawables
+     */
+    fun compoundDrawable(editText: EditText, drawable: Drawable) = editText.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null)
 }
