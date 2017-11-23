a652728c8fa7ab1e1bec287900a7fc00b8ac950a
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-11-23 18:32:22 -0300

    Update attribute.

--- app/src/main/java/chat/rocket/android/app/DateTimeHelper.kt
+++ app/src/main/java/chat/rocket/android/app/DateTimeHelper.kt
@@ -6,13 +6,12 @@ import org.threeten.bp.Period
 import org.threeten.bp.format.DateTimeFormatter
 import org.threeten.bp.format.FormatStyle
 import org.threeten.bp.format.TextStyle
-import org.threeten.bp.temporal.ChronoUnit
 import java.util.*
 
 object DateTimeHelper {
     private val today = LocalDate.now()
     private val yesterday = today.minusDays(1)
-    private val lastWeek = today.minus(1, ChronoUnit.WEEKS)
+    private val lastWeek = today.minusWeeks(1)
 
     /**
      * Returns a date from a LocalDateTime or the textual representation if the LocalDateTime has a max period of a week from the current date.
