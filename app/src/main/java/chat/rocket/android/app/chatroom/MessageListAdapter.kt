4b68973c9853a76b0f6977d25a27e8c0cee430d7
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-12-08 16:54:38 -0300

    Get formatted time from the DateTimeHelper

--- app/src/main/java/chat/rocket/android/app/chatroom/MessageListAdapter.kt
+++ app/src/main/java/chat/rocket/android/app/chatroom/MessageListAdapter.kt
@@ -1,6 +1,6 @@
 package chat.rocket.android.app.chatroom
 
-import android.content.Context
+import DateTimeHelper
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
 import android.view.View
@@ -10,7 +10,7 @@ import chat.rocket.android.R
 import com.facebook.drawee.view.SimpleDraweeView
 import kotlinx.android.synthetic.main.item_message.view.*
 
-class MessageListAdapter(private var dataSet: MutableList<Message>, private val context: Context) : RecyclerView.Adapter<MessageListAdapter.ViewHolder>() {
+class MessageListAdapter(private var dataSet: MutableList<Message>) : RecyclerView.Adapter<MessageListAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.item_message, parent, false)
@@ -22,7 +22,7 @@ class MessageListAdapter(private var dataSet: MutableList<Message>, private val 
 
         holder.userAvatar.setImageURI(message.user.avatarUri)
         holder.userName.text = message.user.name
-        holder.time.text = message.time.toString()
+        holder.time.text = DateTimeHelper.getTime(message.localDatetime)
         holder.content.text = message.content
     }
 
