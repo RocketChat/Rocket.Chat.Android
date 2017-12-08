89363d0f94404b4d9cbde85d4f7f23cf7fc0e769
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-12-08 16:51:18 -0300

    Add todo.

--- app/src/main/java/chat/rocket/android/app/chatlist/Chat.kt
+++ app/src/main/java/chat/rocket/android/app/chatlist/Chat.kt
@@ -6,6 +6,7 @@ import org.threeten.bp.LocalDateTime
 data class Chat(val user: User,
                 val name: String,
                 val type: String,
+                // Todo replace to Message type instead of String
                 val lastMessage: String,
                 val lastMessageDateTime: LocalDateTime,
                 val totalUnreadMessages: Int)
