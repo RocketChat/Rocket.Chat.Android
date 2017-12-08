bb362a598939e4850cd133c9115a44364d792fca
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-12-08 09:47:27 -0300

    Add user attribute.

--- app/src/main/java/chat/rocket/android/app/chatlist/Chat.kt
+++ app/src/main/java/chat/rocket/android/app/chatlist/Chat.kt
@@ -3,6 +3,7 @@ package chat.rocket.android.app.chatlist
 import org.threeten.bp.LocalDateTime
 
 data class Chat(val userAvatarUri: String,
+data class Chat(val user: User,
                 val name: String,
                 val type: String,
                 val userStatus: String?,
