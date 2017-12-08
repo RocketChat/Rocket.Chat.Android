03a7efa7d7a08f55ea00e7a8415a8642d5d56529
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-12-08 09:48:38 -0300

    Update ChatListAdapter.kt

--- app/src/main/java/chat/rocket/android/app/chatlist/ChatListAdapter.kt
+++ app/src/main/java/chat/rocket/android/app/chatlist/ChatListAdapter.kt
@@ -22,7 +22,7 @@ class ChatListAdapter(private var dataSet: MutableList<Chat>, private val contex
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val chat = dataSet[position]
 
-        holder.userAvatar.setImageURI(chat.userAvatarUri)
+        holder.userAvatar.setImageURI(chat.user.avatarUri)
         holder.chatName.text = chat.name
         holder.lastMessage.text = chat.lastMessage
         holder.lastMessageDateTime.text = DateTimeHelper.getDate(chat.lastMessageDateTime, context)
@@ -31,7 +31,7 @@ class ChatListAdapter(private var dataSet: MutableList<Chat>, private val contex
             "p" -> DrawableHelper.compoundDrawable(holder.chatName, DrawableHelper.getDrawableFromId(R.drawable.ic_lock_outline_black, context))
             "c" -> DrawableHelper.compoundDrawable(holder.chatName, DrawableHelper.getDrawableFromId(R.drawable.ic_hashtag_black, context))
             "d" -> {
-                val userStatus = chat.userStatus
+                val userStatus = chat.user.status
                 if (userStatus != null) {
                     DrawableHelper.compoundDrawable(holder.chatName, DrawableHelper.getUserStatusDrawable(userStatus, context))
                 }
