d40c727accf3d067b9bee2e7888a8d6365704846
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-12-20 14:07:48 -0300

    Update ServerView.kt

--- app/src/main/java/chat/rocket/android/authentication/server/presentation/ServerView.kt
+++ app/src/main/java/chat/rocket/android/authentication/server/presentation/ServerView.kt
@@ -1,3 +1,6 @@
 package chat.rocket.android.authentication.server.presentation
 
-interface ServerView
+import chat.rocket.android.core.behaviours.LoadingView
+import chat.rocket.android.core.behaviours.MessageView
+
+interface ServerView : LoadingView, MessageView
