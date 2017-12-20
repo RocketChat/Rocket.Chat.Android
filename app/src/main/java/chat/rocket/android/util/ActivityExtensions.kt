2040decf95628c5cc2e67a8445e626a83bc33ba8
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-12-20 13:22:59 -0300

    Add fragment transaction custom animations.

--- app/src/main/java/chat/rocket/android/util/ActivityExtensions.kt
+++ app/src/main/java/chat/rocket/android/util/ActivityExtensions.kt
@@ -2,6 +2,7 @@ package chat.rocket.android.util
 
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
+import chat.rocket.android.R
 
 fun AppCompatActivity.addFragment(tag: String, layoutId: Int, newInstance: () -> Fragment) {
     val fragment = supportFragmentManager.findFragmentByTag(tag) ?: newInstance()
@@ -10,5 +11,9 @@ fun AppCompatActivity.addFragment(tag: String, layoutId: Int, newInstance: () ->
 
 fun AppCompatActivity.addFragmentBackStack(tag: String, layoutId: Int, newInstance: () -> Fragment) {
     val fragment = supportFragmentManager.findFragmentByTag(tag) ?: newInstance()
-    supportFragmentManager.beginTransaction().replace(layoutId, fragment, tag).addToBackStack(tag).commit()
+    supportFragmentManager.beginTransaction()
+            .setCustomAnimations(R.anim.enter_from_right, R.anim.exit_to_left, R.anim.enter_from_left, R.anim.exit_to_right)
+            .replace(layoutId, fragment, tag)
+            .addToBackStack(tag)
+            .commit()
 }
