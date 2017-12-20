c8fe7a8879f8b4fcaf329b3284824dd1543cd0b1
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-12-20 14:07:58 -0300

    Update ServerFragment.kt

--- app/src/main/java/chat/rocket/android/authentication/server/ui/ServerFragment.kt
+++ app/src/main/java/chat/rocket/android/authentication/server/ui/ServerFragment.kt
@@ -5,44 +5,67 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.view.WindowManager
+import android.view.ViewTreeObserver
+import android.widget.Toast
 import chat.rocket.android.R
 import chat.rocket.android.authentication.server.presentation.ServerPresenter
 import chat.rocket.android.authentication.server.presentation.ServerView
+import chat.rocket.android.helper.KeyboardHelper
+import chat.rocket.android.util.hintContent
 import chat.rocket.android.util.ifEmpty
+import chat.rocket.android.util.setVisibility
+import chat.rocket.android.util.textContent
 import dagger.android.support.AndroidSupportInjection
 import kotlinx.android.synthetic.main.fragment_authentication_server.*
 import javax.inject.Inject
 
 class ServerFragment : Fragment(), ServerView {
-
-    @Inject
-    lateinit var presenter: ServerPresenter
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidSupportInjection.inject(this)
-        super.onCreate(savedInstanceState)
-    }
-
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? =
-            inflater.inflate(R.layout.fragment_authentication_server, container, false)
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        text_server_url.setSelection(text_server_url.length())
-
-        activity?.window?.setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE)
-
-        button_connect.setOnClickListener {
-            val url = text_server_url.text.toString().ifEmpty(text_server_url.hint.toString())
-            presenter.login(server_protocol_label.text.toString() + url)
-        }
+    @Inject lateinit var presenter: ServerPresenter
+    private val layoutListener = ViewTreeObserver.OnGlobalLayoutListener {
+        text_server_url.isCursorVisible = KeyboardHelper.isSoftKeyboardShown(relative_layout.rootView)
     }
 
     companion object {
         fun newInstance() = ServerFragment()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        AndroidSupportInjection.inject(this)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? = inflater.inflate(R.layout.fragment_authentication_server, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        relative_layout.viewTreeObserver.addOnGlobalLayoutListener(layoutListener)
+
+        activity?.applicationContext?.apply {
+            button_connect.setOnClickListener {
+                val url = text_server_url.textContent.ifEmpty(text_server_url.hintContent)
+                presenter.login(this, text_server_protocol.textContent + url)
+            }
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        relative_layout.viewTreeObserver.removeOnGlobalLayoutListener(layoutListener)
+    }
+
+    override fun showLoading() {
+        text_server_url.isEnabled = false
+        button_connect.isEnabled = false
+        view_loading.setVisibility(true)
+    }
+
+    override fun hideLoading() {
+        view_loading.setVisibility(false)
+        button_connect.isEnabled = true
+        text_server_url.isEnabled = true
+    }
+
+    override fun showMessage(message: String) {
+        Toast.makeText(activity, message, Toast.LENGTH_LONG).show()
     }
 }
