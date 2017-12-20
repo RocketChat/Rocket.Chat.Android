c37f08871c204cb5d7217451b1e6697962e288a3
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-12-20 14:05:06 -0300

    Update AuthenticationActivity.kt

--- app/src/main/java/chat/rocket/android/authentication/ui/AuthenticationActivity.kt
+++ app/src/main/java/chat/rocket/android/authentication/ui/AuthenticationActivity.kt
@@ -1,40 +1,39 @@
 package chat.rocket.android.authentication.ui
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import chat.rocket.android.R
-import chat.rocket.android.app.LayoutHelper
 import chat.rocket.android.authentication.server.ui.ServerFragment
+import chat.rocket.android.helper.LayoutHelper
 import chat.rocket.android.util.addFragment
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
 import dagger.android.support.HasSupportFragmentInjector
+import kotlinx.android.synthetic.main.activity_authentication.*
 import javax.inject.Inject
 
 class AuthenticationActivity : AppCompatActivity(), HasSupportFragmentInjector {
-
-    @Inject
-    lateinit var fragmentDispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
-
-    val layoutHelper = LayoutHelper()
+    @Inject lateinit var fragmentDispatchingAndroidInjector: DispatchingAndroidInjector<Fragment>
+    private val layoutHelper = LayoutHelper()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        AndroidInjection.inject(this)
-
         setContentView(R.layout.activity_authentication)
 
+        AndroidInjection.inject(this)
         layoutHelper.install(this)
+
         addFragment("authenticationServerFragment", R.id.fragment_container) {
             ServerFragment.newInstance()
         }
     }
 
     override fun onDestroy() {
-        layoutHelper.remove()
         super.onDestroy()
+        layoutHelper.remove()
     }
 
     override fun supportFragmentInjector(): AndroidInjector<Fragment> {
