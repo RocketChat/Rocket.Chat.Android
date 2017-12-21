8cf42aac0e2640df053a12c3be59c12de7dea708
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-12-21 17:56:42 -0300

    TwoFAView interface implements the MessageView interface

--- app/src/main/java/chat/rocket/android/authentication/twofactor/presentation/TwoFAView.kt
+++ app/src/main/java/chat/rocket/android/authentication/twofactor/presentation/TwoFAView.kt
@@ -1,5 +1,6 @@
 package chat.rocket.android.authentication.twofactor.presentation
 
-import chat.rocket.android.authentication.login.presentation.LoginView
+import chat.rocket.android.core.behaviours.LoadingView
+import chat.rocket.android.core.behaviours.MessageView
 
-interface TwoFAView : LoginView
+interface TwoFAView : LoadingView, MessageView
