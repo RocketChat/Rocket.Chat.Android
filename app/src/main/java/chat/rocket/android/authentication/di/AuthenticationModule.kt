4e2e3faacd9fb3204af7f1fc3298ea1660c45cc3
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-12-24 11:39:06 -0300

    Remove provideAuthTokenRepository function.

--- app/src/main/java/chat/rocket/android/authentication/di/AuthenticationModule.kt
+++ app/src/main/java/chat/rocket/android/authentication/di/AuthenticationModule.kt
@@ -1,6 +1,5 @@
 package chat.rocket.android.authentication.di
 
-import chat.rocket.android.authentication.infraestructure.AuthTokenRepository
 import chat.rocket.android.authentication.presentation.AuthenticationNavigator
 import chat.rocket.android.authentication.ui.AuthenticationActivity
 import chat.rocket.android.dagger.scope.PerActivity
@@ -14,12 +13,6 @@ class AuthenticationModule {
     @Provides
     @PerActivity
     fun provideAuthenticationNavigator(activity: AuthenticationActivity) = AuthenticationNavigator(activity)
-
-    @Provides
-    @PerActivity
-    fun provideAuthTokenRepository(): AuthTokenRepository {
-        return AuthTokenRepository()
-    }
 
     @Provides
     fun provideJob(): Job {
