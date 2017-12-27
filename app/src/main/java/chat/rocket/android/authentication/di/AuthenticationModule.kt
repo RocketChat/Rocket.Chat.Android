ea0781b507025793aaaf6e1ae8845a9a5efe576d
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-12-27 01:58:47 -0300

    Add context parameter.

--- app/src/main/java/chat/rocket/android/authentication/di/AuthenticationModule.kt
+++ app/src/main/java/chat/rocket/android/authentication/di/AuthenticationModule.kt
@@ -1,5 +1,6 @@
 package chat.rocket.android.authentication.di
 
+import android.content.Context
 import chat.rocket.android.authentication.presentation.AuthenticationNavigator
 import chat.rocket.android.authentication.ui.AuthenticationActivity
 import chat.rocket.android.dagger.scope.PerActivity
@@ -12,7 +13,7 @@ class AuthenticationModule {
 
     @Provides
     @PerActivity
-    fun provideAuthenticationNavigator(activity: AuthenticationActivity) = AuthenticationNavigator(activity)
+    fun provideAuthenticationNavigator(activity: AuthenticationActivity, context: Context) = AuthenticationNavigator(activity, context)
 
     @Provides
     fun provideJob(): Job {
