5a77f176b7a6dbe571ead31589c068d4ecd6c5ba
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2017-12-27 20:14:17 -0300

    Remove imports

--- app/src/main/java/chat/rocket/android/authentication/presentation/AuthenticationNavigator.kt
+++ app/src/main/java/chat/rocket/android/authentication/presentation/AuthenticationNavigator.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import android.content.Intent
 import chat.rocket.android.R
 import chat.rocket.android.chatrooms.ui.MainActivity
-import chat.rocket.android.authentication.ui.AuthenticationActivity
-import chat.rocket.android.app.MainActivity
 import chat.rocket.android.authentication.login.ui.LoginFragment
 import chat.rocket.android.authentication.signup.ui.SignupFragment
 import chat.rocket.android.authentication.twofactor.ui.TwoFAFragment
