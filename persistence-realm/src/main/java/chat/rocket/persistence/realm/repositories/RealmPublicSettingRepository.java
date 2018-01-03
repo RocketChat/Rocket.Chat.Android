4535d9ee8860de9654313de48d9b21a2f8b77075
Leonardo Aramaki <leoaramaki@gmail.com>
2018-01-03 18:47:53 -0200

    Return a default empty PublicSetting if server does not provide any

--- persistence-realm/src/main/java/chat/rocket/persistence/realm/repositories/RealmPublicSettingRepository.java
+++ persistence-realm/src/main/java/chat/rocket/persistence/realm/repositories/RealmPublicSettingRepository.java
@@ -5,6 +5,9 @@ import android.support.v4.util.Pair;
 
 import com.hadisatrio.optional.Optional;
 
+import javax.annotation.Nullable;
+
+import chat.rocket.core.PublicSettingsConstants;
 import chat.rocket.core.models.PublicSetting;
 import chat.rocket.core.repositories.PublicSettingRepository;
 import chat.rocket.persistence.realm.RealmStore;
@@ -15,32 +18,44 @@ import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.realm.RealmResults;
 
 public class RealmPublicSettingRepository extends RealmRepository
-    implements PublicSettingRepository {
+        implements PublicSettingRepository {
 
-  private final String hostname;
+    private final String hostname;
 
-  public RealmPublicSettingRepository(String hostname) {
-    this.hostname = hostname;
-  }
+    public RealmPublicSettingRepository(String hostname) {
+        this.hostname = hostname;
+    }
 
-  @Override
-  public Single<Optional<PublicSetting>> getById(String id) {
-    return Single.defer(() -> Flowable.using(
-        () -> new Pair<>(RealmStore.getRealm(hostname), Looper.myLooper()),
-        pair -> {
-            if (pair.first == null) {
-              return Flowable.empty();
-            }
-            return pair.first.where(RealmPublicSetting.class)
-                  .equalTo(RealmPublicSetting.ID, id)
-                  .findAll()
-                  .<RealmResults<RealmPublicSetting>>asFlowable();
-        },
-        pair -> close(pair.first, pair.second)
-    )
-        .unsubscribeOn(AndroidSchedulers.from(Looper.myLooper()))
-        .filter(it -> it.isLoaded() && it.isValid() && it.size() > 0)
-        .map(it -> Optional.of(it.get(0).asPublicSetting()))
-        .first(Optional.absent()));
-  }
+    @Override
+    public Single<Optional<PublicSetting>> getById(String id) {
+        return Single.defer(() -> Flowable.using(
+                () -> new Pair<>(RealmStore.getRealm(hostname), Looper.myLooper()),
+                pair -> {
+                    if (pair.first == null) {
+                        return Flowable.empty();
+                    }
+
+                    return pair.first.where(RealmPublicSetting.class)
+                            .equalTo(RealmPublicSetting.ID, id)
+                            .findAll()
+                            .<RealmResults<RealmPublicSetting>>asFlowable();
+                },
+                pair -> close(pair.first, pair.second)
+        )
+                .unsubscribeOn(AndroidSchedulers.from(Looper.myLooper()))
+                .filter(it -> it.isLoaded() && it.isValid())
+                .map(it -> {
+                    if (it.size() > 0) {
+                        return Optional.of(it.get(0).asPublicSetting());
+                    }
+
+                    PublicSetting defaultSetting = PublicSetting.builder()
+                            .setId(id)
+                            .setValue("")
+                            .setUpdatedAt(0L)
+                            .build();
+                    return Optional.of(defaultSetting);
+                })
+                .first(Optional.absent()));
+    }
 }
