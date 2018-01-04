deb483ded94623bd435bae270a5ae3665af7bf18
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2018-01-04 09:45:19 -0300

    Add getOpenChatRooms and sortChatRooms functions.

--- app/src/main/java/chat/rocket/android/chatrooms/presentation/ChatRoomsPresenter.kt
+++ app/src/main/java/chat/rocket/android/chatrooms/presentation/ChatRoomsPresenter.kt
@@ -4,17 +4,30 @@ import chat.rocket.android.core.lifecycle.CancelStrategy
 import chat.rocket.android.util.launchUI
 import chat.rocket.core.RocketChatClient
 import chat.rocket.core.internal.rest.chatRooms
+import chat.rocket.core.model.ChatRoom
 import javax.inject.Inject
 
 class ChatRoomsPresenter @Inject constructor(private val view: ChatRoomsView, private val strategy: CancelStrategy) {
     @Inject lateinit var client: RocketChatClient
 
-    fun getChatRooms() {
+    fun chatRooms() {
         launchUI(strategy) {
             view.showLoading()
-            // TODO How to get the chat rooms?
-            //view.showChatRooms(client.chatRooms().update.toMutableList())
+            val chatRooms = client.chatRooms().update
+            val openChatRooms = getOpenChatRooms(chatRooms)
+            val sortedOpenChatRooms = sortChatRooms(openChatRooms)
+            view.showChatRooms(sortedOpenChatRooms.toMutableList())
             view.hideLoading()
+        }
+    }
+
+    private fun getOpenChatRooms(chatRooms: List<ChatRoom>): List<ChatRoom> {
+        return chatRooms.filter(ChatRoom::open)
+    }
+
+    private fun sortChatRooms(chatRooms: List<ChatRoom>): List<ChatRoom> {
+        return chatRooms.sortedByDescending {
+            chatRoom -> chatRoom.lastMessage?.timestamp
         }
     }
 }
