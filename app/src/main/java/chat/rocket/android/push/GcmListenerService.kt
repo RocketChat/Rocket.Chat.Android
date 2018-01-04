76425b8dfce2db52ee4ada61fed0b1635059a378
Leonardo Aramaki <leoaramaki@gmail.com>
2018-01-04 21:32:15 -0200

    Add default notification icon

---
+++ app/src/main/java/chat/rocket/android/push/GcmListenerService.kt
@@ -0,0 +1,11 @@
+package chat.rocket.android.push
+
+import android.os.Bundle
+import com.google.android.gms.gcm.GcmListenerService
+
+class GcmListenerService : GcmListenerService() {
+
+    override fun onMessageReceived(from: String?, data: Bundle?) {
+        println("UBA BA!")
+    }
+}
