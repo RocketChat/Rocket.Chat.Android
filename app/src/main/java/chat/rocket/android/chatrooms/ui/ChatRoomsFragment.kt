f495ce5f63c023d8baa787a24ba0a2266156f329
Leonardo Aramaki <leoaramaki@gmail.com>
2018-01-15 16:35:37 -0200

    Add spotlight functionality inside de Toolbar

--- app/src/main/java/chat/rocket/android/chatrooms/ui/ChatRoomsFragment.kt
+++ app/src/main/java/chat/rocket/android/chatrooms/ui/ChatRoomsFragment.kt
@@ -2,12 +2,12 @@ package chat.rocket.android.chatrooms.ui
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.app.AppCompatActivity
 import android.support.v7.util.DiffUtil
 import android.support.v7.widget.DefaultItemAnimator
 import android.support.v7.widget.LinearLayoutManager
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.support.v7.widget.SearchView
+import android.view.*
 import android.widget.Toast
 import chat.rocket.android.R
 import chat.rocket.android.chatrooms.presentation.ChatRoomsPresenter
@@ -15,6 +15,7 @@ import chat.rocket.android.chatrooms.presentation.ChatRoomsView
 import chat.rocket.android.widget.DividerItemDecoration
 import chat.rocket.core.model.ChatRoom
 import dagger.android.support.AndroidSupportInjection
+import kotlinx.android.synthetic.main.app_bar.*
 import kotlinx.android.synthetic.main.fragment_chat_rooms.*
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.android.UI
@@ -23,7 +24,9 @@ import kotlinx.coroutines.experimental.launch
 import javax.inject.Inject
 
 class ChatRoomsFragment : Fragment(), ChatRoomsView {
+
     @Inject lateinit var presenter: ChatRoomsPresenter
+    private var searchView: SearchView? = null
 
     companion object {
         fun newInstance() = ChatRoomsFragment()
@@ -32,6 +35,7 @@ class ChatRoomsFragment : Fragment(), ChatRoomsView {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         AndroidSupportInjection.inject(this)
+        setHasOptionsMenu(true)
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? = inflater.inflate(R.layout.fragment_chat_rooms, container, false)
@@ -39,22 +43,38 @@ class ChatRoomsFragment : Fragment(), ChatRoomsView {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        floating_search_view.setOnQueryChangeListener { oldQuery, newQuery ->
-            floating_search_view.showProgress()
-            presenter.chatRoomsByName(newQuery)
-            if (oldQuery.isNotEmpty() && newQuery.isEmpty()) {
-                floating_search_view.clearSuggestions()
-                floating_search_view.hideProgress()
-            }
-        }
-
-        activity?.apply {
+        (activity as AppCompatActivity).apply {
             recycler_view.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
             recycler_view.addItemDecoration(DividerItemDecoration(this, 144, 32))
             recycler_view.adapter = ChatRoomsAdapter(this)
             recycler_view.itemAnimator = DefaultItemAnimator()
+            if (supportActionBar == null) {
+                setSupportActionBar(toolbar)
+                supportActionBar?.setDisplayHomeAsUpEnabled(false)
+                supportActionBar?.setDisplayShowHomeEnabled(false)
+                supportActionBar?.setDisplayShowTitleEnabled(false)
+            }
         }
         presenter.loadChatRooms()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
+        inflater?.inflate(R.menu.chatrooms_menu, menu)
+        val searchItem = menu?.findItem(R.id.action_search)
+
+        searchView = searchItem?.actionView as SearchView
+
+        val sv = searchView
+        sv?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return queryChatRoomsByName(query)
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return queryChatRoomsByName(newText)
+            }
+
+        })
     }
 
     /*override fun showChatRooms(dataSet: MutableList<ChatRoom>) {
@@ -69,7 +89,6 @@ class ChatRoomsFragment : Fragment(), ChatRoomsView {
                     DiffUtil.calculateDiff(RoomsDiffCallback(adapter.dataSet, newDataSet))
                 }.await()
 
-                floating_search_view.hideProgress()
                 adapter.updateRooms(newDataSet)
                 diff.dispatchUpdatesTo(adapter)
             }
@@ -83,6 +102,11 @@ class ChatRoomsFragment : Fragment(), ChatRoomsView {
     override fun showMessage(message: String) = Toast.makeText(activity, message, Toast.LENGTH_SHORT).show()
 
     override fun showGenericErrorMessage() = showMessage(getString(R.string.msg_generic_error))
+
+    private fun queryChatRoomsByName(name: String?): Boolean {
+        presenter.chatRoomsByName(name ?: "")
+        return true
+    }
 
     class RoomsDiffCallback(private val oldRooms: List<ChatRoom>,
                             private val newRooms: List<ChatRoom>) : DiffUtil.Callback() {
