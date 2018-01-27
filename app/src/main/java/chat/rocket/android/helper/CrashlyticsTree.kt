44475941b2f80c000024360d7e401164a4561788
Lucio Maciel <luciofm@gmail.com>
2018-01-26 22:19:18 -0300

    Add nullability in CrashlyticsTree

--- app/src/main/java/chat/rocket/android/helper/CrashlyticsTree.kt
+++ app/src/main/java/chat/rocket/android/helper/CrashlyticsTree.kt
@@ -6,7 +6,7 @@ import timber.log.Timber
 
 class CrashlyticsTree : Timber.Tree() {
 
-    override fun log(priority: Int, tag: String, message: String, throwable: Throwable?) {
+    override fun log(priority: Int, tag: String?, message: String?, throwable: Throwable?) {
         Crashlytics.log(priority, tag, message)
 
         if (throwable != null) {
