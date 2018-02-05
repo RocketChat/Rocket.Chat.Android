4d499f5bcfc6b2fd1757936c946f9322bf4cde90
Leonardo Aramaki <leoaramaki@gmail.com>
2018-02-05 13:45:34 -0200

    Get a ChatRoom object by its room id

--- app/src/main/java/chat/rocket/android/server/domain/GetChatRoomsInteractor.kt
+++ app/src/main/java/chat/rocket/android/server/domain/GetChatRoomsInteractor.kt
@@ -1,6 +1,7 @@
 package chat.rocket.android.server.domain
 
 import chat.rocket.core.model.ChatRoom
+import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.async
 import javax.inject.Inject
 
@@ -18,5 +19,22 @@ class GetChatRoomsInteractor @Inject constructor(private val repository: ChatRoo
             }
         }
         return chatRooms.await()
+    }
+
+    /**
+     * Get a specific room by its id.
+     *
+     * @param serverUrl The server url where the room is.
+     * @param roomId The id of the room to get.
+     *
+     * @return The ChatRoom object or null if we couldn't find any.
+     */
+    suspend fun getByRoomId(serverUrl: String, roomId: String): ChatRoom {
+        return async(CommonPool) {
+            val allChatRooms = repository.get(serverUrl)
+            return@async allChatRooms.first {
+                it.id == roomId
+            }
+        }.await()
     }
 }
