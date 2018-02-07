0749391e31f007907748edcb1c88c4b13f12c315
Leonardo Aramaki <leoaramaki@gmail.com>
2018-02-07 21:04:28 -0200

    Fix return type from mapper function

--- app/src/main/java/chat/rocket/android/chatroom/viewmodel/MessageViewModelMapper.kt
+++ app/src/main/java/chat/rocket/android/chatroom/viewmodel/MessageViewModelMapper.kt
@@ -7,7 +7,7 @@ import chat.rocket.core.TokenRepository
 import chat.rocket.core.model.Message
 import chat.rocket.core.model.Value
 import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.launch
+import kotlinx.coroutines.experimental.withContext
 import javax.inject.Inject
 
 class MessageViewModelMapper @Inject constructor(private val context: Context,
@@ -15,8 +15,15 @@ class MessageViewModelMapper @Inject constructor(private val context: Context,
                                                  private val messageParser: MessageParser,
                                                  private val messagesRepository: MessagesRepository) {
 
-    suspend fun mapToViewModel(message: Message, settings: Map<String, Value<Any>>?) = launch(CommonPool) {
-        MessageViewModel(this@MessageViewModelMapper.context, tokenRepository.get(), message, settings, messageParser, messagesRepository)
+    suspend fun mapToViewModel(message: Message, settings: Map<String, Value<Any>>?): MessageViewModel = withContext(CommonPool) {
+        MessageViewModel(
+                this@MessageViewModelMapper.context,
+                tokenRepository.get(),
+                message,
+                settings,
+                messageParser,
+                messagesRepository
+        )
     }
 
     suspend fun mapToViewModelList(messageList: List<Message>, settings: Map<String, Value<Any>>?): List<MessageViewModel> {
