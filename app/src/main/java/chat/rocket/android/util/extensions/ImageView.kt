48ce2c429c8c3615159ff954d4313a7492a8cd1f
Lucio Maciel <luciofm@gmail.com>
2018-02-26 10:24:41 -0300

    Simplify glide calling

--- app/src/main/java/chat/rocket/android/util/extensions/ImageView.kt
+++ app/src/main/java/chat/rocket/android/util/extensions/ImageView.kt
@@ -6,8 +6,5 @@ import chat.rocket.android.core.GlideApp
 import chat.rocket.android.core.GlideRequest
 
 fun ImageView.setImageURI(url: String?, block: GlideRequest<Drawable>.() -> Unit = { this }) {
-    GlideApp.with(this).load(url).apply {
-        this.block()
-        this
-    }.into(this)
+    GlideApp.with(this).load(url).apply(block).into(this)
 }
