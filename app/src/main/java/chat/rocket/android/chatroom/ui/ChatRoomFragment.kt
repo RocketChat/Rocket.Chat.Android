09a9a63ceb7884c629bd15fed1f568e7aa272d0a
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2018-03-13 18:55:27 -0300

    Fixes the keyboard shown when opening the member list view.

--- app/src/main/java/chat/rocket/android/chatroom/ui/ChatRoomFragment.kt
+++ app/src/main/java/chat/rocket/android/chatroom/ui/ChatRoomFragment.kt
@@ -133,6 +133,11 @@ class ChatRoomFragment : Fragment(), ChatRoomView, EmojiKeyboardListener, EmojiR
         presenter.unsubscribeMessages(chatRoomId)
         handler.removeCallbacksAndMessages(null)
         unsubscribeTextMessage()
+
+        // Hides the keyboard (it it' opened) before going to any view.
+        activity?.apply {
+            hideKeyboard()
+        }
         super.onDestroyView()
     }
 
