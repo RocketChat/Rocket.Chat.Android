1c9017ae036690a79f447d8eed15b12561efef7a
aniketsingh03 <aniketsingh.sachan@gmail.com>
2018-03-15 09:29:39 +0530

    minor fixes

--- app/src/main/java/chat/rocket/android/createChannel/addMembers/ui/AddMembersActivity.kt
+++ app/src/main/java/chat/rocket/android/createChannel/addMembers/ui/AddMembersActivity.kt
@@ -1,8 +1,13 @@
 package chat.rocket.android.createChannel.addMembers.ui
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import chat.rocket.android.R
 import chat.rocket.android.createChannel.addMembers.presentation.AddMembersPresenter
 import chat.rocket.android.createChannel.addMembers.presentation.AddMembersView
+import com.jakewharton.rxbinding2.widget.RxTextView
+import kotlinx.android.synthetic.main.activity_create_new_channel.*
+import kotlinx.android.synthetic.main.layout_toolbar.*
 import javax.inject.Inject
 
 class AddMembersActivity : AppCompatActivity(), AddMembersView {
@@ -28,4 +33,23 @@ class AddMembersActivity : AppCompatActivity(), AddMembersView {
 
     @Inject
     lateinit var presenter: AddMembersPresenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setUpToolBar()
+    }
+
+    private fun setUpToolBar(){
+        toolbar_title.text = getString(R.string.title_add_members)
+        toolbar_action_text.text = getString(R.string.action_select_members)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        RxTextView.textChanges(channel_name_edit_text).subscribe { text ->
+            toolbar_action_text.isEnabled = text.isNotEmpty()
+            if (text.isEmpty())
+                toolbar_action_text.alpha = 0.8f
+            else
+                toolbar_action_text.alpha = 1.0f
+        }
+    }
 }
