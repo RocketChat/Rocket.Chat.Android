15b6c4902b34719afe89b06a971d9e6e684eba96
Aniket Singh <aniketsingh.sachan@gmail.com>
2018-04-03 14:52:45 +0530

    adds code to create channel

--- app/src/main/java/chat/rocket/android/createChannel/presentation/CreateNewChannelPresenter.kt
+++ app/src/main/java/chat/rocket/android/createChannel/presentation/CreateNewChannelPresenter.kt
@@ -1,7 +1,34 @@
 package chat.rocket.android.createChannel.presentation
 
+import chat.rocket.android.core.lifecycle.CancelStrategy
+import chat.rocket.android.server.domain.GetCurrentServerInteractor
+import chat.rocket.android.server.infraestructure.RocketChatClientFactory
+import chat.rocket.android.util.extensions.launchUI
+import chat.rocket.common.RocketChatException
+import chat.rocket.common.model.RoomType
+import chat.rocket.core.RocketChatClient
+import chat.rocket.core.internal.rest.createChannel
 import javax.inject.Inject
 
 
-class CreateNewChannelPresenter @Inject constructor() {
+class CreateNewChannelPresenter @Inject constructor(private val view: CreateNewChannelView,
+                                                    private val strategy: CancelStrategy,
+                                                    private val serverInteractor: GetCurrentServerInteractor,
+                                                    factory: RocketChatClientFactory) {
+    private val client: RocketChatClient = factory.create(serverInteractor.get()!!)
+
+    fun createNewChannel(roomType: RoomType, channelName: String, usersList: List<String>, readOnly: Boolean) {
+        launchUI(strategy) {
+            view.showLoading()
+            try {
+                client.createChannel(roomType, channelName, usersList, readOnly)
+                view.showMessage("Channel Created successfully")
+            } catch (ex: RocketChatException) {
+                //TODO remove this and add Log message instead
+                ex.printStackTrace()
+            } finally {
+                view.hideLoading()
+            }
+        }
+    }
 }
--- app/src/main/java/chat/rocket/android/createChannel/ui/CreateNewChannelActivity.kt
+++ app/src/main/java/chat/rocket/android/createChannel/ui/CreateNewChannelActivity.kt
@@ -4,9 +4,12 @@ import android.graphics.drawable.GradientDrawable
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.MenuItem
+import android.widget.Toast
 import chat.rocket.android.R
 import chat.rocket.android.createChannel.presentation.CreateNewChannelPresenter
 import chat.rocket.android.createChannel.presentation.CreateNewChannelView
+import chat.rocket.android.util.extensions.setVisible
+import chat.rocket.common.model.roomTypeOf
 import com.jakewharton.rxbinding2.widget.RxTextView
 import dagger.android.AndroidInjection
 import kotlinx.android.synthetic.main.activity_create_new_channel.*
@@ -16,11 +19,15 @@ import javax.inject.Inject
 
 class CreateNewChannelActivity : AppCompatActivity(), CreateNewChannelView {
     override fun showLoading() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        view_loading.setVisible(true)
+        layout_container.alpha = 0.5f
+        layout_container.isEnabled = false
     }
 
     override fun hideLoading() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        view_loading.setVisible(false)
+        layout_container.alpha = 1.0f
+        layout_container.isEnabled = true
     }
 
     override fun showMessage(resId: Int) {
@@ -28,7 +35,7 @@ class CreateNewChannelActivity : AppCompatActivity(), CreateNewChannelView {
     }
 
     override fun showMessage(message: String) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 
     override fun showGenericErrorMessage() {
@@ -66,16 +73,17 @@ class CreateNewChannelActivity : AppCompatActivity(), CreateNewChannelView {
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         RxTextView.textChanges(channel_name_edit_text).subscribe { text ->
             toolbar_action_text.isEnabled = text.isNotEmpty()
-            if (text.isEmpty())
+            if (text.isEmpty()) {
                 toolbar_action_text.alpha = 0.8f
-            else
+            } else {
                 toolbar_action_text.alpha = 1.0f
+            }
         }
     }
 
     private fun setUpOnClickListeners() {
         public_channel.setOnClickListener {
-            channelType = "public"
+            channelType = "c"
 
             channel_type.text = getString(R.string.public_channel_type)
             channel_description.text = getString(R.string.private_channel_type_description)
@@ -93,7 +101,7 @@ class CreateNewChannelActivity : AppCompatActivity(), CreateNewChannelView {
         }
 
         private_channel.setOnClickListener {
-            channelType = "private"
+            channelType = "p"
 
             channel_type.text = getString(R.string.private_channel_type)
             channel_description.text = getString(R.string.public_channel_description)
@@ -109,6 +117,12 @@ class CreateNewChannelActivity : AppCompatActivity(), CreateNewChannelView {
 
             private_channel.setTextColor(resources.getColor(R.color.colorLightTheme))
             public_channel.setTextColor(resources.getColor(R.color.red))
+        }
+
+        toolbar_action_text.setOnClickListener {
+            if (toolbar_action_text.isEnabled) {
+                presenter.createNewChannel(roomTypeOf(channelType), channel_name_edit_text.text.toString(), listOf("aniket03"), false)
+            }
         }
     }
 }
