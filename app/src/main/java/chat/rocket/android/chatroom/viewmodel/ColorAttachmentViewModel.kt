63bc35a563a4e7a99c412936f2d458955d5e799d
Lucio Maciel <luciofm@gmail.com>
2018-04-14 01:14:31 -0300

    Implement isTemporary in ColorAttachmentViewModel

--- app/src/main/java/chat/rocket/android/chatroom/viewmodel/ColorAttachmentViewModel.kt
+++ app/src/main/java/chat/rocket/android/chatroom/viewmodel/ColorAttachmentViewModel.kt
@@ -14,7 +14,8 @@ data class ColorAttachmentViewModel(
     override val messageId: String,
     override var reactions: List<ReactionViewModel>,
     override var nextDownStreamMessage: BaseViewModel<*>? = null,
-    override var preview: Message? = null
+    override var preview: Message? = null,
+    override var isTemporary: Boolean = false
 ) : BaseAttachmentViewModel<ColorAttachment> {
     override val viewType: Int
         get() = BaseViewModel.ViewType.COLOR_ATTACHMENT.viewType
