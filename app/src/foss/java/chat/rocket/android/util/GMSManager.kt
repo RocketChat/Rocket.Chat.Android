991580e02d2c3a091e94f8727b9431feb7343cd2
Marcus Hoffmann <bubu@bubu1.eu>
2018-04-30 18:45:30 +0200

    implement Foss build flavour
    Provide two build flavours 'Foss' and 'Play', the 'Play' flavour is
    unchanged to what we had before. The 'Foss' flavour removes the
    dependence on proprietary libraries.
    
    This removes crashlytics, instead logging to the local log.
    
    It also removes firebase/gcm messaging, disabling push functionality.
    For now there is no replacement for this.

---
+++ app/src/foss/java/chat/rocket/android/util/GMSManager.kt
@@ -0,0 +1,13 @@
+package chat.rocket.android.util
+
+
+import chat.rocket.android.main.ui.MainActivity
+import chat.rocket.android.main.presentation.MainPresenter
+
+fun refreshFCMToken(presenter: MainPresenter) {
+    //Do absolutely nothing
+}
+
+fun invalidateFirebaseToken(token: String) {
+    //Do absolutely nothing
+}
