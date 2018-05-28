cf9c9165cbbddc6e43a5fff9d66f036025b06c1a
Lucio Maciel <luciofm@gmail.com>
2018-05-28 07:26:31 -0300

    Remove realm migration

--- app/src/main/java/chat/rocket/android/app/RocketChatApplication.kt
+++ app/src/main/java/chat/rocket/android/app/RocketChatApplication.kt
@@ -1,14 +1,5 @@
 package chat.rocket.android.app
 
-import android.app.Activity
-import android.app.Application
-import android.app.Service
-import androidx.lifecycle.ProcessLifecycleOwner
-import android.content.BroadcastReceiver
-import android.content.Context
-import android.content.SharedPreferences
-import androidx.core.content.edit
-import chat.rocket.android.BuildConfig
 /*import chat.rocket.android.app.migration.RealmMigration
 import chat.rocket.android.app.migration.RocketChatLibraryModule
 import chat.rocket.android.app.migration.RocketChatServerModule
@@ -16,30 +7,39 @@ import chat.rocket.android.app.migration.model.RealmBasedServerInfo
 import chat.rocket.android.app.migration.model.RealmPublicSetting
 import chat.rocket.android.app.migration.model.RealmSession
 import chat.rocket.android.app.migration.model.RealmUser*/
+import android.app.Activity
+import android.app.Application
+import android.app.Service
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import androidx.lifecycle.ProcessLifecycleOwner
+import chat.rocket.android.BuildConfig
 import chat.rocket.android.authentication.domain.model.toToken
 import chat.rocket.android.dagger.DaggerAppComponent
 import chat.rocket.android.dagger.qualifier.ForMessages
 import chat.rocket.android.helper.CrashlyticsTree
 import chat.rocket.android.infrastructure.LocalRepository
-import chat.rocket.android.server.domain.*
-import chat.rocket.android.server.domain.model.Account
-import chat.rocket.android.util.extensions.avatarUrl
-import chat.rocket.android.util.extensions.serverLogoUrl
+import chat.rocket.android.server.domain.GetAccountsInteractor
+import chat.rocket.android.server.domain.GetCurrentServerInteractor
+import chat.rocket.android.server.domain.MultiServerTokenRepository
+import chat.rocket.android.server.domain.SettingsRepository
+import chat.rocket.android.server.domain.TokenRepository
 import chat.rocket.android.widget.emoji.EmojiRepository
 import chat.rocket.common.model.Token
-import chat.rocket.core.model.Value
 import com.crashlytics.android.Crashlytics
 import com.crashlytics.android.core.CrashlyticsCore
 import com.facebook.drawee.backends.pipeline.DraweeConfig
 import com.facebook.drawee.backends.pipeline.Fresco
 import com.facebook.imagepipeline.core.ImagePipelineConfig
 import com.jakewharton.threetenabp.AndroidThreeTen
-import dagger.android.*
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
+import dagger.android.HasBroadcastReceiverInjector
+import dagger.android.HasServiceInjector
 import io.fabric.sdk.android.Fabric
-import io.realm.Realm
-import io.realm.RealmConfiguration
-import kotlinx.coroutines.experimental.CommonPool
-import kotlinx.coroutines.experimental.launch
 import kotlinx.coroutines.experimental.runBlocking
 import timber.log.Timber
 import java.lang.ref.WeakReference
@@ -74,10 +74,6 @@ class RocketChatApplication : Application(), HasActivityInjector, HasServiceInje
     lateinit var settingsRepository: SettingsRepository
     @Inject
     lateinit var tokenRepository: TokenRepository
-    @Inject
-    lateinit var accountRepository: AccountsRepository
-    @Inject
-    lateinit var saveCurrentServerRepository: SaveCurrentServerInteractor
     @Inject
     lateinit var prefs: SharedPreferences
     @Inject
@@ -119,118 +115,7 @@ class RocketChatApplication : Application(), HasActivityInjector, HasServiceInje
             localRepository.setOldMessagesCleanedUp()
         }
 
-        // TODO - remove this and all realm stuff when we got to 80% in 2.0
-        try {
-            if (!localRepository.hasMigrated()) {
-                migrateFromLegacy()
-            }
-        } catch (ex: Exception) {
-            Timber.d(ex, "Error migrating old accounts")
-        }
-    }
-
-    private fun migrateFromLegacy() {
-        Realm.init(this)
-        val serveListConfiguration = RealmConfiguration.Builder()
-                .name("server.list.realm")
-                .schemaVersion(6)
-                .migration(RealmMigration())
-                .modules(RocketChatServerModule())
-                .build()
-
-        val serverRealm = Realm.getInstance(serveListConfiguration)
-        val serversInfoList = serverRealm.where(RealmBasedServerInfo::class.java).findAll().toList()
-        serversInfoList.forEach { server ->
-            val hostname = server.hostname
-            val url = if (server.insecure) "http://$hostname" else "https://$hostname"
-
-            val config = RealmConfiguration.Builder()
-                    .name("${server.hostname}.realm")
-                    .schemaVersion(6)
-                    .migration(RealmMigration())
-                    .modules(RocketChatLibraryModule())
-                    .build()
-
-            val realm = Realm.getInstance(config)
-            val user = realm.where(RealmUser::class.java)
-                    .isNotEmpty(RealmUser.EMAILS).findFirst()
-            val session = realm.where(RealmSession::class.java).findFirst()
-
-            migratePublicSettings(url, realm)
-            if (user != null && session != null) {
-                val authToken = session.token
-                settingsRepository.get(url)
-                migrateServerInfo(url, authToken!!, settingsRepository.get(url), user)
-            }
-            realm.close()
-        }
-        migrateCurrentServer(serversInfoList)
-        serverRealm.close()
-        localRepository.setMigrated(true)
-    }
-
-    private fun migrateServerInfo(url: String, authToken: String, settings: PublicSettings, user: RealmUser) {
-        val userId = user._id
-        val avatar = url.avatarUrl(user.username!!)
-        val icon = settings.favicon()?.let {
-            url.serverLogoUrl(it)
-        }
-        val logo = settings.wideTile()?.let {
-            url.serverLogoUrl(it)
-        }
-        val account = Account(url, icon, logo, user.username!!, avatar)
-        launch(CommonPool) {
-            tokenRepository.save(url, Token(userId!!, authToken))
-            accountRepository.save(account)
-        }
-    }
-
-    private fun migratePublicSettings(url: String, realm: Realm) {
-        val settings = realm.where(RealmPublicSetting::class.java).findAll()
-
-        val serverSettings = hashMapOf<String, Value<Any>>()
-        settings.toList().forEach { setting ->
-            val type = setting.type!!
-            val value = setting.value!!
-
-            val convertedSetting = when (type) {
-                "string" -> Value(value)
-                "language" -> Value(value)
-                "boolean" -> Value(value.toBoolean())
-                "int" -> try {
-                    Value(value.toInt())
-                } catch (ex: NumberFormatException) {
-                    Value(0)
-                }
-                else -> null // ignore
-            }
-
-            if (convertedSetting != null) {
-                val id = setting._id!!
-                serverSettings.put(id, convertedSetting)
-            }
-        }
-        settingsRepository.save(url, serverSettings)
-    }
-
-    private fun migrateCurrentServer(serversList: List<RealmBasedServerInfo>) {
-        if (getCurrentServerInteractor.get() == null) {
-            var currentServer = getSharedPreferences("cache", Context.MODE_PRIVATE)
-                    .getString("KEY_SELECTED_SERVER_HOSTNAME", null)
-
-            currentServer = if (serversList.isNotEmpty()) {
-                val server = serversList.find { it.hostname == currentServer }
-                val hostname = server!!.hostname
-                if (server.insecure) {
-                    "http://$hostname"
-                } else {
-                    "https://$hostname"
-                }
-            } else {
-                "http://$currentServer"
-            }
-            saveCurrentServerRepository.save(currentServer)
-        }
+        // TODO - remove REALM files.
     }
 
     private fun migrateInternalTokens() {
@@ -293,11 +178,6 @@ class RocketChatApplication : Application(), HasActivityInjector, HasServiceInje
     }
 }
 
-private fun LocalRepository.setMigrated(migrated: Boolean) {
-    save(LocalRepository.MIGRATION_FINISHED_KEY, migrated)
-}
-
-private fun LocalRepository.hasMigrated() = getBoolean(LocalRepository.MIGRATION_FINISHED_KEY)
 private fun LocalRepository.needOldMessagesCleanUp() = getBoolean(CLEANUP_OLD_MESSAGES_NEEDED, true)
 private fun LocalRepository.setOldMessagesCleanedUp() = save(CLEANUP_OLD_MESSAGES_NEEDED, false)
 
---
+++ app/src/main/java/chat/rocket/android/chatrooms/adapter/RoomMapper.kt
@@ -0,0 +1,2 @@
+package chat.rocket.android.chatrooms.adapter
+
