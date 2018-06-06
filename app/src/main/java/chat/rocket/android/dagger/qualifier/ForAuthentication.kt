73eb4e8932496c169a9c201e712f69d8f7018394
Lucio Maciel <luciofm@gmail.com>
2018-06-06 14:32:33 -0300

    Remove created by comment

--- app/src/main/java/chat/rocket/android/dagger/qualifier/ForAuthentication.kt
+++ app/src/main/java/chat/rocket/android/dagger/qualifier/ForAuthentication.kt
@@ -2,10 +2,6 @@ package chat.rocket.android.dagger.qualifier
 
 import javax.inject.Qualifier
 
-/**
- * Created by luciofm on 4/14/18.
- */
-
 @Qualifier
 @Retention(AnnotationRetention.RUNTIME)
 annotation class ForAuthentication
