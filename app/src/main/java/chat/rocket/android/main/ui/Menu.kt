e19030676e6a33edf6616179b65da4c1d8b14ba4
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2018-08-10 15:54:05 -0300

    Using with to avoid repeating menu lots of time.

--- app/src/main/java/chat/rocket/android/main/ui/Menu.kt
+++ app/src/main/java/chat/rocket/android/main/ui/Menu.kt
@@ -5,54 +5,56 @@ import android.view.MenuItem
 import chat.rocket.android.R
 
 internal fun MainActivity.setupMenu(menu: Menu) {
-    menu.add(
-        R.id.menu_section_one,
-        R.id.menu_action_chats,
-        Menu.NONE,
-        R.string.title_chats
-    ).setIcon(R.drawable.ic_chat_bubble_black_24dp)
-        .isChecked = true
-
-    menu.add(
-        R.id.menu_section_one,
-        R.id.menu_action_create_channel,
-        Menu.NONE,
-        R.string.action_create_channel
-    ).setIcon(R.drawable.ic_create_black_24dp)
-
-    menu.add(
-        R.id.menu_section_two,
-        R.id.menu_action_profile,
-        Menu.NONE,
-        R.string.title_profile
-    ).setIcon(R.drawable.ic_person_black_24dp)
-
-    menu.add(
-        R.id.menu_section_two,
-        R.id.menu_action_settings,
-        Menu.NONE,
-        R.string.title_settings
-    ).setIcon(R.drawable.ic_settings_black_24dp)
-
-    if (permissions.canSeeTheAdminPanel()) {
-        menu.add(
-            R.id.menu_section_two,
-            R.id.menu_action_admin_panel,
+    with(menu) {
+        add(
+            R.id.menu_section_one,
+            R.id.menu_action_chats,
             Menu.NONE,
-            R.string.title_admin_panel
+            R.string.title_chats
+        ).setIcon(R.drawable.ic_chat_bubble_black_24dp)
+            .isChecked = true
+
+        add(
+            R.id.menu_section_one,
+            R.id.menu_action_create_channel,
+            Menu.NONE,
+            R.string.action_create_channel
+        ).setIcon(R.drawable.ic_create_black_24dp)
+
+        add(
+            R.id.menu_section_two,
+            R.id.menu_action_profile,
+            Menu.NONE,
+            R.string.title_profile
+        ).setIcon(R.drawable.ic_person_black_24dp)
+
+        add(
+            R.id.menu_section_two,
+            R.id.menu_action_settings,
+            Menu.NONE,
+            R.string.title_settings
         ).setIcon(R.drawable.ic_settings_black_24dp)
+
+        if (permissions.canSeeTheAdminPanel()) {
+            add(
+                R.id.menu_section_two,
+                R.id.menu_action_admin_panel,
+                Menu.NONE,
+                R.string.title_admin_panel
+            ).setIcon(R.drawable.ic_settings_black_24dp)
+        }
+
+        add(
+            R.id.menu_section_three,
+            R.id.menu_action_logout,
+            Menu.NONE,
+            R.string.action_logout
+        ).setIcon(R.drawable.ic_logout_black_24dp)
+
+        setGroupCheckable(R.id.menu_section_one, true, true)
+        setGroupCheckable(R.id.menu_section_two, true, true)
+        setGroupCheckable(R.id.menu_section_three, true, true)
     }
-
-    menu.add(
-        R.id.menu_section_three,
-        R.id.menu_action_logout,
-        Menu.NONE,
-        R.string.action_logout
-    ).setIcon(R.drawable.ic_logout_black_24dp)
-
-    menu.setGroupCheckable(R.id.menu_section_one, true, true)
-    menu.setGroupCheckable(R.id.menu_section_two, true, true)
-    menu.setGroupCheckable(R.id.menu_section_three, true, true)
 }
 
 internal fun MainActivity.onNavDrawerItemSelected(menuItem: MenuItem) {
