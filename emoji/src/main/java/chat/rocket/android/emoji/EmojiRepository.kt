f3292176395824787dab2a92362ec0c10d4a6235
Leonardo Aramaki <leoaramaki@gmail.com>
2018-09-12 12:12:29 -0300

    Add support for custom emojis on reactions

--- emoji/src/main/java/chat/rocket/android/emoji/EmojiRepository.kt
+++ emoji/src/main/java/chat/rocket/android/emoji/EmojiRepository.kt
@@ -197,7 +197,7 @@ object EmojiRepository {
         }
     }
 
-    internal fun getCustomEmojis(): List<Emoji> = customEmojis
+    fun getCustomEmojis(): List<Emoji> = customEmojis
 
     /**
      * Get all recently used emojis ordered by usage count.
