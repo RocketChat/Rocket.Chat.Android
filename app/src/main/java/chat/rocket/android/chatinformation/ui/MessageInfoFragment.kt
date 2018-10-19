03b64eb2411f623d9406760ff10cdfd67e597c36
divyanshu <divyanshub024@gmail.com>
2018-10-19 18:55:30 +0530

    minor changes

--- app/src/main/java/chat/rocket/android/chatinformation/ui/MessageInfoFragment.kt
+++ app/src/main/java/chat/rocket/android/chatinformation/ui/MessageInfoFragment.kt
@@ -55,9 +55,9 @@ class MessageInfoFragment : Fragment(), MessageInfoView {
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View {
         return inflater.inflate(R.layout.fragment_message_info, container, false)
     }
