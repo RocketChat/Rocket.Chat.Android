7a5dc36a0f2fdfa9077f09957a7350698f4d9256
shubhsherl <dhoni.singh094@gmail.com>
2018-10-20 10:31:27 +0530

    change logout message and title

--- app/src/main/java/chat/rocket/android/main/presentation/MainPresenter.kt
+++ app/src/main/java/chat/rocket/android/main/presentation/MainPresenter.kt
@@ -166,6 +166,7 @@ class MainPresenter @Inject constructor(
 
     fun showLogoutDialog(context: Context) {
         val builder = AlertDialog.Builder(context)
+        builder.setTitle(R.string.action_logout)
         builder.setMessage(R.string.title_confirmation)
         builder.setPositiveButton(R.string.action_logout) { _, _ -> logout()}
             .setNegativeButton(R.string.action_stay) { dialog, _ -> dialog.cancel() }
