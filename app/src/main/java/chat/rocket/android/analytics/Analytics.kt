a4be9a73efd94a94bbeeadb61b75e0c10b6c2bc9
Filipe de Lima Brito <filipedelimabrito@gmail.com>
2018-11-14 13:49:48 -0800

    Updates Analytics.kt

--- app/src/main/java/chat/rocket/android/analytics/Analytics.kt
+++ app/src/main/java/chat/rocket/android/analytics/Analytics.kt
@@ -12,7 +12,7 @@ interface Analytics {
      * @param event The [AuthenticationEvent] used to log in.
      * @param loginSucceeded True if successful logged in, false otherwise.
      */
-    fun logLogin(event: AuthenticationEvent, loginSucceeded: Boolean)
+    fun logLogin(event: AuthenticationEvent, loginSucceeded: Boolean) {}
 
     /**
      * Logs the sign up event.
@@ -20,14 +20,14 @@ interface Analytics {
      * @param event The [AuthenticationEvent] used to sign up.
      * @param signUpSucceeded True if successful signed up, false otherwise.
      */
-    fun logSignUp(event: AuthenticationEvent, signUpSucceeded: Boolean)
+    fun logSignUp(event: AuthenticationEvent, signUpSucceeded: Boolean) {}
 
     /**
      * Logs the screen view event.
      *
      * @param event The [ScreenViewEvent] to log.
      */
-    fun logScreenView(event: ScreenViewEvent)
+    fun logScreenView(event: ScreenViewEvent) {}
 
     /**
      * Logs the message sent event.
@@ -35,7 +35,7 @@ interface Analytics {
      * @param event The [SubscriptionTypeEvent] to log.
      * @param serverUrl The server URL to log.
      */
-    fun logMessageSent(event: SubscriptionTypeEvent, serverUrl: String)
+    fun logMessageSent(event: SubscriptionTypeEvent, serverUrl: String) {}
 
     /**
      * Logs the media upload event.
@@ -43,14 +43,14 @@ interface Analytics {
      * @param event The [SubscriptionTypeEvent] to log.
      * @param mimeType The mime type of the media uploaded to log.
      */
-    fun logMediaUploaded(event: SubscriptionTypeEvent, mimeType: String)
+    fun logMediaUploaded(event: SubscriptionTypeEvent, mimeType: String) {}
 
     /**
      * Logs the reaction event.
      *
      * @param event The [SubscriptionTypeEvent] to log.
      */
-    fun logReaction(event: SubscriptionTypeEvent)
+    fun logReaction(event: SubscriptionTypeEvent) {}
 
     /**
      * Logs the server switch event.
@@ -58,17 +58,17 @@ interface Analytics {
      * @param serverUrl The server URL to log.
      * @param serverCount The number of server(s) the use own.
      */
-    fun logServerSwitch(serverUrl: String, serverCount: Int)
+    fun logServerSwitch(serverUrl: String, serverCount: Int) {}
 
     /**
      * Logs the admin opening event.
      */
-    fun logOpenAdmin()
+    fun logOpenAdmin() {}
 
     /**
      * Logs the reset password event.
      *
      * @param resetPasswordSucceeded True if successful reset password, false otherwise.
      */
-    fun logResetPassword(resetPasswordSucceeded: Boolean)
+    fun logResetPassword(resetPasswordSucceeded: Boolean) {}
 }
