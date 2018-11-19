73b96d7a0d0356413a7917364989cbd5deed2ad2
erosenthal <eric.rosenthal@viasat.com>
2018-11-19 13:58:20 -0500

    comment

--- app/src/main/java/chat/rocket/android/helper/Constants.kt
+++ app/src/main/java/chat/rocket/android/helper/Constants.kt
@@ -12,6 +12,7 @@ object Constants {
     const val CHATROOM_LIVE_CHAT = 3
 
     // Enables/disables WIDECHAT specific features, functionality and views
+    // Use both WIDECHAT and WIDECHAT_DEV switches == true to allow for normal RC login sequence and login to any server
     const val WIDECHAT = true
     const val WIDECHAT_DEV = false
 }
