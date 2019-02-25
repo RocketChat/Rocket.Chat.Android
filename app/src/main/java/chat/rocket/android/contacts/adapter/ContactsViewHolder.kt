26bf2ea134ae4928cd22b1951c1ff7bd8c38c75f
erosenthal <eric.rosenthal@viasat.com>
2019-02-25 11:27:04 -0500

    fix bug that populates contact detail incorrectly after scrolling

--- app/src/main/java/chat/rocket/android/contacts/adapter/ContactsViewHolder.kt
+++ app/src/main/java/chat/rocket/android/contacts/adapter/ContactsViewHolder.kt
@@ -37,6 +37,7 @@ class ContactsViewHolder(itemView: View) : ViewHolder<ContactsItemHolder>(itemVi
                 }
 
             } else {
+                contact_detail.isGone = true
                 invite_contact.isGone = true
                 chat_username.isVisible = true
                 chat_username.text = "@${contact.getUsername()}"
