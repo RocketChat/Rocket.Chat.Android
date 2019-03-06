8f0e94c766c033f2613b4a3ac33b0f64a873f017
Asel Peiris <peirisasel373@gmail.com>
2019-03-06 23:36:48 +0530

    Implementation of splash screen

---
+++ app/src/main/java/chat/rocket/android/splash/SplashScreenActivity.kt
@@ -0,0 +1,17 @@
+package chat.rocket.android.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import chat.rocket.android.authentication.ui.AuthenticationActivity
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val intent = Intent(this, AuthenticationActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+} 
