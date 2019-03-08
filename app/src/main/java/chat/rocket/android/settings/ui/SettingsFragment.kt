240979abcb78b5e75d7e638bc53fa4b312342ec2
Divyanshu Bhargava <divyanshub024@gmail.com>
2019-03-08 21:07:09 +0530

    add import

--- app/src/main/java/chat/rocket/android/settings/ui/SettingsFragment.kt
+++ app/src/main/java/chat/rocket/android/settings/ui/SettingsFragment.kt
@@ -2,6 +2,7 @@ package chat.rocket.android.settings.ui
 
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
