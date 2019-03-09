878305eacb1caaeae15f72f418112158688325e1
Asel Peiris <peirisasel373@gmail.com>
2019-03-09 10:59:31 +0530

    Reformatting code

--- app/src/main/java/chat/rocket/android/chatroom/ui/Menu.kt
+++ app/src/main/java/chat/rocket/android/chatroom/ui/Menu.kt
@@ -4,12 +4,10 @@ import android.content.Context
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.core.content.res.ResourcesCompat
 import chat.rocket.android.R
 import chat.rocket.android.util.extension.onQueryTextListener
-import chat.rocket.android.util.extensions.showToast
 
 internal fun ChatRoomFragment.setupMenu(menu: Menu) {
     setupSearchMessageMenuItem(menu, requireContext())
@@ -20,46 +18,44 @@ internal fun ChatRoomFragment.setupMenu(menu: Menu) {
 internal fun ChatRoomFragment.setOnMenuItemClickListener(item: MenuItem) {
     when (item.itemId) {
         MENU_ACTION_FAVORITE_UNFAVOURITE_CHAT -> presenter.toggleFavoriteChatRoom(
-            chatRoomId,
-            isFavorite
+                chatRoomId,
+                isFavorite
         )
         MENU_ACTION_SHOW_DETAILS -> presenter.toChatDetails(
-            chatRoomId,
-            chatRoomType,
-            isSubscribed,
-            disableMenu
+                chatRoomId,
+                chatRoomType,
+                isSubscribed,
+                disableMenu
         )
     }
 }
 
 private fun ChatRoomFragment.setupSearchMessageMenuItem(menu: Menu, context: Context) {
     val searchItem = menu.add(
-        Menu.NONE,
-        Menu.NONE,
-        Menu.NONE,
-        R.string.title_search_message
+            Menu.NONE,
+            Menu.NONE,
+            Menu.NONE,
+            R.string.title_search_message
     ).setActionView(SearchView(context))
-        .setIcon(R.drawable.ic_search_white_24dp)
-        .setShowAsActionFlags(
-            MenuItem.SHOW_AS_ACTION_IF_ROOM or MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW
-        )
-        .setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+            .setIcon(R.drawable.ic_search_white_24dp)
+            .setShowAsActionFlags(
+                    MenuItem.SHOW_AS_ACTION_IF_ROOM or MenuItem.SHOW_AS_ACTION_COLLAPSE_ACTION_VIEW
+            )
+            .setOnActionExpandListener(object : MenuItem.OnActionExpandListener {
+                override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
+                    dismissEmojiKeyboard()
+                    removeFavoriteMenuItem(menu)
+                    removeDetailMenuItem(menu)
+                    return true
+                }
 
-
-            override fun onMenuItemActionExpand(item: MenuItem?): Boolean {
-                dismissEmojiKeyboard()
-                removeFavoriteMenuItem(menu)
-                removeDetailMenuItem(menu)
-                return true
-            }
-
-            override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
-                dismissEmojiKeyboard()
-                setupFavoriteMenuItem(menu)
-                setupDetailsMenuItem(menu)
-                return true
-            }
-        })
+                override fun onMenuItemActionCollapse(item: MenuItem?): Boolean {
+                    dismissEmojiKeyboard()
+                    setupFavoriteMenuItem(menu)
+                    setupDetailsMenuItem(menu)
+                    return true
+                }
+            })
 
     (searchItem?.actionView as? SearchView)?.let {
         // TODO: Check why we need to stylize the search text programmatically instead of by defining it in the styles.xml (ChatRoom.SearchView)
@@ -75,7 +71,7 @@ private fun stylizeSearchView(searchView: SearchView, context: Context) {
     val searchText = searchView.findViewById<EditText>(androidx.appcompat.R.id.search_src_text)
     searchText.setTextColor(ResourcesCompat.getColor(context.resources, R.color.color_white, null))
     searchText.setHintTextColor(
-        ResourcesCompat.getColor(context.resources, R.color.color_white, null)
+            ResourcesCompat.getColor(context.resources, R.color.color_white, null)
     )
 }
 
@@ -94,20 +90,20 @@ private fun ChatRoomFragment.setupSearchViewTextListener(searchView: SearchView)
 private fun ChatRoomFragment.setupFavoriteMenuItem(menu: Menu) {
     if (isFavorite) {
         menu.add(
-            Menu.NONE,
-            MENU_ACTION_FAVORITE_UNFAVOURITE_CHAT,
-            Menu.NONE,
-            R.string.title_unfavorite_chat
+                Menu.NONE,
+                MENU_ACTION_FAVORITE_UNFAVOURITE_CHAT,
+                Menu.NONE,
+                R.string.title_unfavorite_chat
         ).setIcon(R.drawable.ic_star_yellow_24dp)
-            .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM)
     } else {
         menu.add(
-            Menu.NONE,
-            MENU_ACTION_FAVORITE_UNFAVOURITE_CHAT,
-            Menu.NONE,
-            R.string.title_favorite_chat
+                Menu.NONE,
+                MENU_ACTION_FAVORITE_UNFAVOURITE_CHAT,
+                Menu.NONE,
+                R.string.title_favorite_chat
         ).setIcon(R.drawable.ic_star_border_white_24dp)
-            .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM)
+                .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM)
     }
 }
 
@@ -121,14 +117,10 @@ private fun ChatRoomFragment.setupDetailsMenuItem(menu: Menu) {
             .setShowAsAction(MenuItem.SHOW_AS_ACTION_IF_ROOM)
 }
 
-private fun removeFavoriteMenuItem(menu: Menu){
+private fun removeFavoriteMenuItem(menu: Menu) {
     menu.removeItem(MENU_ACTION_FAVORITE_UNFAVOURITE_CHAT)
 }
 
-private fun removeDetailMenuItem(menu: Menu){
+private fun removeDetailMenuItem(menu: Menu) {
     menu.removeItem(MENU_ACTION_SHOW_DETAILS)
 }
-
-
-
-
