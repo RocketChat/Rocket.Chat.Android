58d4c40edd127646b94aed73524cfaafd1145bd5
Hussein El Feky <hussein.feky@gmail.com>
2019-04-18 21:06:44 +0200

    Removed wildcard import

--- app/src/main/java/chat/rocket/android/profile/ui/ProfileFragment.kt
+++ app/src/main/java/chat/rocket/android/profile/ui/ProfileFragment.kt
@@ -6,7 +6,11 @@ import android.content.Intent
 import android.graphics.Bitmap
 import android.os.Build
 import android.os.Bundle
-import android.view.*
+import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
 import android.widget.RadioGroup
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
