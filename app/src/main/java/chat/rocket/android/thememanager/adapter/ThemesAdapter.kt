c55efdb3211df1a8aaf65e4dba9705c8fafe1a83
Noor Binte Amir <noor.binte.amir@gmail.com>
2019-06-23 18:23:38 +0500

    [Improvement] Completed UI for ThemeFragment.kt

--- app/src/main/java/chat/rocket/android/thememanager/adapter/ThemesAdapter.kt
+++ app/src/main/java/chat/rocket/android/thememanager/adapter/ThemesAdapter.kt
@@ -1,5 +1,6 @@
 package chat.rocket.android.thememanager.adapter
 
+import android.graphics.Color
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
@@ -7,7 +8,7 @@ import chat.rocket.android.R
 import chat.rocket.android.thememanager.model.Theme
 import chat.rocket.android.util.extensions.inflate
 import kotlinx.android.synthetic.main.item_theme_row.view.*
-
+import android.graphics.drawable.GradientDrawable
 
 class ThemesAdapter(private val themes: List<Theme>, private val listener: (Theme) -> Unit) : RecyclerView.Adapter<ThemesAdapter.ViewHolder>() {
 
@@ -26,7 +27,26 @@ class ThemesAdapter(private val themes: List<Theme>, private val listener: (Them
 
     class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
         fun bind(theme : Theme){
+            theme.setPreviewColors(itemView.context)
             itemView.text_theme_name.text = theme.toString()
+
+            val drawablePrimaryText = DrawableHelper.getDrawableFromId(R.drawable.theme_preview, itemView.context).mutate()
+            val drawableBackground = DrawableHelper.getDrawableFromId(R.drawable.theme_preview, itemView.context).mutate()
+            val drawableAccent = DrawableHelper.getDrawableFromId(R.drawable.theme_preview, itemView.context).mutate()
+            val drawablePrimary = DrawableHelper.getDrawableFromId(R.drawable.theme_preview, itemView.context).mutate()
+            val drawableDescriptiveText = DrawableHelper.getDrawableFromId(R.drawable.theme_preview, itemView.context).mutate()
+
+            DrawableHelper.tintDrawable(drawableBackground,itemView.context, theme.colorPreviewBackground)
+            DrawableHelper.tintDrawable(drawablePrimaryText,itemView.context, theme.colorPreviewPrimaryText)
+            DrawableHelper.tintDrawable(drawableAccent,itemView.context, theme.colorPreviewAccent)
+            DrawableHelper.tintDrawable(drawablePrimary,itemView.context, theme.colorPreviewPrimary)
+            DrawableHelper.tintDrawable(drawableDescriptiveText,itemView.context, theme.colorPreviewDescriptiveText)
+
+            itemView.item_theme_preview_primary_text.setImageDrawable(drawablePrimaryText)
+            itemView.item_theme_preview_color_background.setImageDrawable(drawableBackground)
+            itemView.item_theme_preview_color_accent.setImageDrawable(drawableAccent)
+            itemView.item_theme_preview_color_primary.setImageDrawable(drawablePrimary)
+            itemView.item_theme_preview_color_descriptive_text.setImageDrawable(drawableDescriptiveText)
         }
     }
 }
--- app/src/main/java/chat/rocket/android/thememanager/model/Theme.kt
+++ app/src/main/java/chat/rocket/android/thememanager/model/Theme.kt
@@ -1,8 +1,29 @@
 package chat.rocket.android.thememanager.model
 
-data class Theme(val name: String) {
+import android.content.Context
+import androidx.annotation.StyleableRes
+
+data class Theme(val name: String, val colorArray: Int) {
+    var colorPreviewBackground = 0
+    var colorPreviewPrimary = 0
+    var colorPreviewAccent = 0
+    var colorPreviewDescriptiveText = 0
+    var colorPreviewPrimaryText = 0
+    @StyleableRes
+    var index = 0
 
     override fun toString(): String {
         return name
+    }
+
+    fun setPreviewColors(context: Context) {
+        val typedArray = context.resources.obtainTypedArray(colorArray)
+        index=0
+        colorPreviewAccent = typedArray.getResourceId(index++, 0)
+        colorPreviewPrimaryText = typedArray.getResourceId(index++, 0)
+        colorPreviewPrimary = typedArray.getResourceId(index++, 0)
+        colorPreviewDescriptiveText = typedArray.getResourceId(index++, 0)
+        colorPreviewBackground = typedArray.getResourceId(index++, 0)
+        typedArray.recycle()
     }
 }
