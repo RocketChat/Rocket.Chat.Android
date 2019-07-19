1991c0601763c26127c593b0318bba22d8a6326e
Govind Dixit <lit2017018@iiita.ac.in>
2019-07-19 15:45:25 +0530

    tests: Directory module

--- app/src/main/java/testConfig/Config.kt
+++ app/src/main/java/testConfig/Config.kt
@@ -1,5 +1,7 @@
 package testConfig
 
+import chat.rocket.android.BuildConfig
+
 class Config {
     companion object {
         const val ORG_NAME: String = "Rocket.Chat"
@@ -11,6 +13,18 @@ class Config {
         const val PASSWORD: String = "123456"
         const val NAME: String = "user121"
         const val EMAIL: String = "qasdf@gmail.com"
+
+        //Existing User
+        const val EXISTING_USER: String ="1marc"
+
+        //Non existing User
+        const val NON_EXISTING_USER: String = "**33##&&"
+
+        //Existing Channel
+        const val EXISTING_CHANNEL: String ="general"
+
+        //Non existing Channel
+        const val NON_EXISTING_CHANNEL: String = "**33##&&"
 
         const val CODE = "1234"
         const val USER_ID = "user_id"
@@ -26,12 +40,23 @@ class Config {
         const val PRIVACY_POLICY: String = "Privacy Policy"
         const val termsOfServiceUrl: String = "$currentServer/terms-of-service"
         const val privacyPolicyUrl: String = "$currentServer/privacy-policy"
+        const val ADMIN_PANEL_URL = "$currentServer/admin/info?layout=embedded"
+        const val LICENSE_URL = "https://github.com/RocketChat/Rocket.Chat.Android/blob/develop/LICENSE"
+        const val LICENSE = "LICENSE"
         const val CHANGE_STATUS: String = "CHANGE STATUS"
         const val ONLINE: String = "Online"
         const val BUSY: String = "Busy"
         const val AWAY: String = "Away"
         const val INVISIBLE: String = "Invisible"
         const val MEMBERS: String = "Members"
+        const val CHANNELS: String = "Channels"
+        const val DIRECT_MESSAGES: String = "Direct Messages"
+        const val FILES: String = "Files"
+        const val USERS: String = "Users"
+        const val DIRECTORY: String = "Directory"
+        const val VERSION_NAME: String = BuildConfig.VERSION_NAME
+        const val VERSION_CODE: Int = BuildConfig.VERSION_CODE
+        const val APP_VERSION: String = "Version: $VERSION_NAME ($VERSION_CODE)"
         const val FAVORITE_MESSAGES: String = "Favorite Messages"
         const val MENTIONS: String = "Mentions"
     }
