49ba6a6a6aa87b8e978ebd8b63787b18a6033a3d
Filipe Brito <filipedelimabrito@gmail.com>
2019-09-24 11:03:14 -0300

    Update schema version

--- app/src/main/java/chat/rocket/android/db/RCDatabase.kt
+++ app/src/main/java/chat/rocket/android/db/RCDatabase.kt
@@ -32,7 +32,7 @@ import chat.rocket.android.emoji.internal.db.StringListConverter
         ReactionEntity::class,
         MessagesSync::class
     ],
-    version = 14,
+    version = 15,
     exportSchema = true
 )
 @TypeConverters(StringListConverter::class)
